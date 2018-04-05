a44a5041b504e893bb131fdd50bdb336c507afae
Tomer Ghelber <tomergelber@gmail.com>
2018-04-05 16:37:32 +0300

    Created HyperTerm for easier usage

--- src/main/scala/relentless/matching/Pattern.scala
+++ src/main/scala/relentless/matching/Pattern.scala
@@ -4,31 +4,40 @@ import relentless.rewriting.HyperEdge
 
 import scala.collection.immutable
 
+abstract class HyperTerm {
+  type HyperTermId = Int
+
+  abstract def value: Option[HyperTermId]
+}
+object HyperTerm {
+  type HyperTermId = Int
+}
+
+case class RealHyperTerm(realVaHyperlue: HyperTerm.HyperTermId) extends HyperTerm {
+  override def value: Option[HyperTermId] = Option(realValue)
+}
+
+case class ValuationHyperTerm(index:Int,  valuation: IndexedSeq[Int]) extends HyperTerm {
+  override def value: Option[HyperTermId] = {
+    val c = valuation(index)
+    if (c == 0) {
+      Option.empty
+    } else {
+      Option(c)
+    }
+  }
+}
+
 /**
   * @author user
   * @since 4/2/2018
-  * @param pattern is negative when pointing to valuation, otherwise its the real value.
-  * @param valuation it positive when has a real value, othwewise its empty
+  * @param transformetdPattern The pattern with terms
   */
-case class Pattern(pattern: IndexedSeq[Int], valuation: Array[Int]) extends immutable.IndexedSeq[Int] {
-  override def length: Int = pattern.length
+case class Pattern(transformetdPattern: IndexedSeq[HyperTerm], valuation: IndexedSeq[Int]) extends immutable.IndexedSeq[Int] {
 
-  type TermId = Int
+  override def length: Int = transformetdPattern.length
 
-  /** Also ~pointer - the logic not on integer.
-   *
-   * @param pointer Points to valuation.
-   * @return Translation for the pointer.
-   */
-  private def translate(pointer: TermId): TermId = -pointer - 1
-
-  /**
-   * Taking the value - if needed going to  valuation
-    * TODO: Replace this with Option - it more logical to use it here.
-   * @param placeholder The placeholder from pattern.
-   * @return The value of the placeholder if any. (bigger than 0).
-   */
-  private def evaluate(placeholder: TermId): TermId = if (placeholder >= 0) placeholder else valuation(translate(-placeholder - 1))
+  import HyperTerm.HyperTermId
 
   /**
     * Matches a word against a pattern. The word has concrete letters, whereas the pattern
@@ -39,28 +48,29 @@ case class Pattern(pattern: IndexedSeq[Int], valuation: Array[Int]) extends immu
     * @return a new valuation, possibly with more assignments set, if the word matches;
     *         otherwise None.
     */
-  def unify(word: HyperEdge[TermId]): Option[Array[TermId]] = {
-    if (word.length != pattern.length)
+  def unify(word: HyperEdge[HyperTermId]): Option[IndexedSeq[HyperTermId]] = {
+    if (word.length != transformetdPattern.length)
       None
     else {
-      def matches(letter: TermId, placeholder: TermId): Option[(Int, TermId)] = {
-        if (placeholder >= 0) { // If its a real value
-          if (letter == placeholder) Some((-1, -1)) else None
-        } else { // if its a pointer to value
-          val vidx = translate(placeholder)
-          val otherLetter = valuation(vidx)
-          if (otherLetter == 0) { //  Unassigned holes
-            Some((vidx, letter)) // positive vidx
-          } else {
-            if (letter == otherLetter)  // if we got bad value.
-              Some((-1, -1)) else None
+      def matches(letter: HyperTermId, term: HyperTerm): Option[(Int, HyperTermId)] = {
+        term match {
+          case RealHyperTerm(placeholder) => if (letter == placeholder) // if we got bad value.
+            Some((-1, -1))
+          else None
+          case ValuationHyperTerm(vidx, _) => {
+            term.value match {
+              case None => Some((vidx, letter)) // Unassigned holes. positive vidx
+              case Some(otherLetter) => if (letter == otherLetter) // if we got bad value.
+                Some((-1, -1))
+              else None
+            }
           }
         }
       }
 
-      val `valuation'`: Array[Int] = valuation.clone()
-      for ((letter, placeholder) <- word zip pattern) {
-        matches(letter, placeholder) match {
+      val `valuation'`: Array[Int] = valuation.toArray
+      for ((letter, term) <- word zip transformetdPattern) {
+        matches(letter, term) match {
           case None => return None // TODO: Should we throw an error and catch it letter?
           case Some((vidx, newLetter)) => if (vidx >= 0)  //  a new valuation, possibly with more assignments set.
             `valuation'`(vidx) = newLetter
@@ -70,18 +80,28 @@ case class Pattern(pattern: IndexedSeq[Int], valuation: Array[Int]) extends immu
     }
   }
 
-  def lookup(hyperTerm: Trie[TermId, HyperEdge[TermId]]): Seq[HyperEdge[TermId]] = {
+  def lookup(hyperTerm: Trie[HyperTermId, HyperEdge[HyperTermId]]): Seq[HyperEdge[HyperTermId]] = {
     var t = hyperTerm
     try {
-      for ((ph, idx) <- pattern.zipWithIndex) {
-        val c = evaluate(ph)
-        if (c > 0) t = t.get(idx, c) getOrElse {
-          return Seq.empty
+      for ((term, idx) <- transformetdPattern.zipWithIndex) {
+        term.value match {
+          case None => return Seq.empty
+          case Some(c) => if (c > 0) t = t.get(idx, c) getOrElse {
+            return Seq.empty
+          }
         }
       }
-      return t.words
+      t.words
     } catch {
-      case e: RuntimeException => throw new RuntimeException(s"matching pattern = ${pattern mkString " "}, valuation = ${valuation mkString " "}; ${e}")
+      case e: RuntimeException => throw new RuntimeException(s"matching pattern = ${transformetdPattern mkString " "}, valuation = ${valuation mkString " "}; ${e}")
     }
   }
+}
+
+case object Pattern {
+  /**
+    * @param pattern is negative when pointing to valuation, otherwise its the real value.
+    * @param valuation it positive when has a real value, othwewise its empty
+    */
+  def apply(pattern: IndexedSeq[Int], valuation: IndexedSeq[Int]) = Pattern(pattern map(ph=>if (ph>=0) RealHyperTerm(ph) else ValuationHyperTerm(~ph, valuation)), valuation)
 }
