baafb20b93166e9088ab4a714fb78593959a4896
Tomer Ghelber <tomergelber@gmail.com>
2018-04-08 17:07:03 +0300

    moved bundle outside of encoding

---
+++ src/main/scala/relentless/matching/Bundle.scala
@@ -0,0 +1,61 @@
+package relentless.matching
+
+import scala.collection.mutable.ListBuffer
+
+
+/**
+ * A sequence of tuples, possibly with holes denoted by negative integers.
+ */
+class Bundle(val tuples: List[Array[Int]]) {
+  
+  def this(tuples: List[Array[Int]], holes: Int*) = //this(tuples)
+    this(Bundle.puncture(tuples, holes.toList))
+  
+  def fillIn(tuple: Array[Int], args: Int*) =
+    tuple map (x => if (x < 0) args(~x) else x)
+  
+  def fillIn(args: Int*): List[Array[Int]] =
+    tuples map (tp => fillIn(tp, args:_*))
+
+  lazy val minValuationSize = tuples.flatten.toSeq.min match {
+    case x if x < 0 => ~x + 1
+    case _ => 0
+  }
+  
+  def bare = new Bundle(tuples filter (_ exists (_ < 0)))
+  
+  import collection.mutable
+  
+  def shuffles = {
+    0 until tuples.length map { i =>
+      val inb = new ListBuffer ++ tuples
+      val outb = new ListBuffer[Array[Int]]
+      val covered = mutable.Set.empty[Int]  // set of placeholders already encountered
+      var j = i
+      while (inb.nonEmpty) {
+        val next = inb(j)
+        inb.remove(j)
+        covered ++= next filter (_ < 0)
+        outb += next
+        // - find the next spot that shares at least one value with covered
+        if (inb.nonEmpty) {
+          j = inb indexWhere (_ exists covered.contains)
+          if (j < 0) j = 0
+            //throw new RuntimeException("bundle is not connected: " + (tuples map (_ mkString ",") mkString " "))
+        }
+      }
+      new Bundle(outb.toList)
+    } toList
+  }
+}
+
+object Bundle {
+  
+  def puncture(tuples: List[Array[Int]], holes: List[Int]): List[Array[Int]] =
+    tuples map (tp => puncture(tp, holes)) filterNot (_(1) <= -2)
+    // drop hole leaves (_(1) <= -2)
+  
+  def puncture(tuple: Array[Int], holes: List[Int]): Array[Int] =
+    tuple map (x => holes.indexOf(x) match { case i if i >= 0 => -i - 2 case _ => x })
+    // note: -1 is reserved, holes are -2 and below
+}
--- src/main/scala/relentless/matching/Encoding.scala
+++ src/main/scala/relentless/matching/Encoding.scala
@@ -3,24 +3,21 @@ package relentless.matching
 import com.typesafe.scalalogging.LazyLogging
 import relentless.rewriting.HyperEdge
 import report.data.NumeratorWithMap
-import syntax.AstSugar._
+import syntax.AstSugar.Term
 import syntax.Tree
-
-import scala.collection.mutable.ListBuffer
-
 
 
 class Encoding extends LazyLogging {
-  
+
   import semantics.LambdaCalculus.isApp
 
   val ntor = new NumeratorWithMap { }
-  
+
   def toTuples(term: Term) : List[HyperEdge[Int]] = toTuples(term, term)
-  
+
   /**
-   * Like toTuples(term), but encodes the top-level node according to equateWith, rather than term.
-   */
+    * Like toTuples(term), but encodes the top-level node according to equateWith, rather than term.
+    */
   def toTuples(term: Term, equateWith: Term) : List[HyperEdge[Int]] = {
     val top = equateWith
     val (head, rest) = headRest(term)
@@ -28,8 +25,8 @@ class Encoding extends LazyLogging {
   }
 
   /**
-   * Like toTuples(term), but encodes the top-level node according to equateWith, rather than term (int version).
-   */
+    * Like toTuples(term), but encodes the top-level node according to equateWith, rather than term (int version).
+    */
   def toTuples(term: Term, equateWith: Int) : List[HyperEdge[Int]] = {
     val top = equateWith
     val (head, rest) = headRest(term)
@@ -47,46 +44,46 @@ class Encoding extends LazyLogging {
   }
 
   /**
-   * Encodes a term with "holes": these are assigned negative integers ~1 through ~holes.length.
-   * Subterms are considered implicit holes, so they are assigned distinct negative values.
-   * The roots of the terms in the bundle are special: they are all encoded as ~0.
-   */
+    * Encodes a term with "holes": these are assigned negative integers ~1 through ~holes.length.
+    * Subterms are considered implicit holes, so they are assigned distinct negative values.
+    * The roots of the terms in the bundle are special: they are all encoded as ~0.
+    */
   def toBundle(holes: Term*)(terms: Term*) = {
     def dbg(x: List[Array[Int]]) { logger.info(s"${x map (_ map (x => if (x < 0) x else (ntor <-- x)) mkString " ")}") }
-    
+
     val altsq = terms.head :: holes.toList ++ (terms flatMap (term => term.nodes filterNot (n => (n eq term) || n.isLeaf)))
     val alt = altsq.zipWithIndex.toMap.mapValues(~_) ++ (terms.tail map ((_, ~0)))
     new Bundle((terms flatMap (toTuples(_, alt, holes.length)) toList))// |-- dbg)
   }
-  
+
   /**
-   * Encodes several bundles together in a way that does not induce encoding collisions
-   * for the placeholder elements.
-   * 
-   * The first bundle is translated normally with its root encoded as ~0.
-   * The holes are encoded ~1 through ~holes.length.
-   * Remaining inner terms and bundle roots are encoded as ~(holes.length+1) onward.
-   * 
-   * NOTICE BIG This function is currently used for rule patterns, whereas toBundle is used
-   *   for regular terms and conclusion schemes. As a result, the `alt` set of terms mapped
-   *   to negative integers include (non-hole) leaves such as "tt" and "nil"; see (*) below.
-   *   Be aware of this small difference when calling them.
-   */
+    * Encodes several bundles together in a way that does not induce encoding collisions
+    * for the placeholder elements.
+    *
+    * The first bundle is translated normally with its root encoded as ~0.
+    * The holes are encoded ~1 through ~holes.length.
+    * Remaining inner terms and bundle roots are encoded as ~(holes.length+1) onward.
+    *
+    * NOTICE BIG This function is currently used for rule patterns, whereas toBundle is used
+    *   for regular terms and conclusion schemes. As a result, the `alt` set of terms mapped
+    *   to negative integers include (non-hole) leaves such as "tt" and "nil"; see (*) below.
+    *   Be aware of this small difference when calling them.
+    */
   def toBundles(holes: Term*)(terms: List[List[Term]]) = {
-    val altsq = terms.head.head :: holes.toList ++ 
+    val altsq = terms.head.head :: holes.toList ++
       (terms.flatten flatMap (term => term.nodes filterNot (n => (n eq term) || (holes contains n) /* (*) */ /*n.isLeaf*/)))
     val alt = altsq.zipWithIndex.toMap.mapValues(~_) ++ (terms.head.tail map ((_, ~0))) ++
       (terms.tail.zipWithIndex flatMap { case (terms, i) => terms map ((_, ~(altsq.length + i))) })
     new Bundle((terms.flatten flatMap (toTuples(_, alt, holes.length)) toList))// |-- dbg)
   }
-  
+
   def headRest(term: Term) = {
     isApp(term) match {
       case Some((f, args)) if f.isLeaf => (f.leaf, args)
       case _ => (term.root, term.subtrees)
     }
   }
-  
+
   def toTuple(sq: Seq[AnyRef]) = HyperEdge(sq map (ntor -->))
   def toTuple(sq: Seq[Term], alt: Map[Term, Int]) = sq map (k => alt.getOrElse(k, ntor --> k)) toArray
 
@@ -94,69 +91,4 @@ class Encoding extends LazyLogging {
     case t: Tree[_] => t.asInstanceOf[Term] // hopefully there are no other trees
     //case _ => T(I("some error", "marker"))
   }
-    
-
 }
-
-
-/**
- * A sequence of tuples, possibly with holes denoted by negative integers.
- */
-class Bundle(val tuples: List[Array[Int]]) {
-  
-  def this(tuples: List[Array[Int]], holes: Int*) = //this(tuples)
-    this(Bundle.puncture(tuples, holes.toList))
-  
-  def fillIn(tuple: Array[Int], args: Int*) =
-    tuple map (x => if (x < 0) args(~x) else x)
-  
-  def fillIn(args: Int*): List[Array[Int]] =
-    tuples map (tp => fillIn(tp, args:_*))
-
-  lazy val minValuationSize = tuples.flatten.toSeq.min match {
-    case x if x < 0 => ~x + 1
-    case _ => 0
-  }
-  
-  def bare = new Bundle(tuples filter (_ exists (_ < 0)))
-  
-  import collection.mutable
-  
-  def shuffles = {
-    0 until tuples.length map { i =>
-      val inb = new ListBuffer ++ tuples
-      val outb = new ListBuffer[Array[Int]]
-      val covered = mutable.Set.empty[Int]  // set of placeholders already encountered
-      var j = i
-      while (inb.nonEmpty) {
-        val next = inb(j)
-        inb.remove(j)
-        covered ++= next filter (_ < 0)
-        outb += next
-        // - find the next spot that shares at least one value with covered
-        if (inb.nonEmpty) {
-          j = inb indexWhere (_ exists covered.contains)
-          if (j < 0) j = 0
-            //throw new RuntimeException("bundle is not connected: " + (tuples map (_ mkString ",") mkString " "))
-        }
-      }
-      new Bundle(outb.toList)
-    } toList
-  }
-}
-
-object Bundle {
-  
-  def puncture(tuples: List[Array[Int]], holes: List[Int]): List[Array[Int]] =
-    tuples map (tp => puncture(tp, holes)) filterNot (_(1) <= -2)
-    // drop hole leaves (_(1) <= -2)
-  
-  def puncture(tuple: Array[Int], holes: List[Int]): Array[Int] =
-    tuple map (x => holes.indexOf(x) match { case i if i >= 0 => -i - 2 case _ => x })
-    // note: -1 is reserved, holes are -2 and below
-}
-
-
-
-
-
