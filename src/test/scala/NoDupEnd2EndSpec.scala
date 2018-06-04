3b90cf0341a6d63e61e38748954717243c3fed93
Eytan Singher <eytan.singher@gmail.com>
2018-06-04 13:03:36 +0300

    Testing: Continued NoDupEnd2End which now fails on empty elaborate output. Spec not finished yet.

--- src/test/scala/NoDupEnd2EndSpec.scala
+++ src/test/scala/NoDupEnd2EndSpec.scala
@@ -5,24 +5,37 @@ import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.read.ListAppender
 import org.scalatest.concurrent.{Signaler, ThreadSignaler}
 import org.scalatest.time.{Millis, Span}
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers, Suite}
+import org.scalatest._
 import org.slf4j.LoggerFactory
 import ui.Interpreter
+import ch.qos.logback.classic.Level
+
 import scala.collection.JavaConverters._
 
 
-class NoDupEnd2EndSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
+class NoDupEnd2EndSpec extends FlatSpec with Matchers with BeforeAndAfterAll with BeforeAndAfter {
   import NoDupEnd2EndSpec._
 
   val timeLimit: Span = Span(2000000, Millis)
   implicit val signaler: Signaler = ThreadSignaler
 
+  var logs: List[ILoggingEvent] = appender.list.asScala.toList
+
+  before {
+    logs = appender.list.asScala.toList.filter(p => p.getLevel == Level.INFO)
+  }
+
+  override protected def beforeAll(): Unit = {
+    Interpreter.main(Array(Interpreter.getClass.getResource("/examples/NoDup.tc").getPath))
+  }
+
   def getVars(msg: String) = {
-    "^\\(([^(]*)\\)".r.findFirstMatchIn(msg.trim).get.group(0).split(" ")
+    "^\\(([^(]*)\\)".r.findFirstMatchIn(msg.trim).get.group(1).split(" ")
   }
 
   def getInput(logs: Seq[ILoggingEvent], index: Int) = {
-    logs(index - 2)
+    if (logs(index - 1).getFormattedMessage.trim.startsWith("DeductionHints")) logs(index - 2)
+    else logs(index - 1)
   }
 
   def getOutput(logs: Seq[ILoggingEvent], index: Int) = {
@@ -34,25 +47,56 @@ class NoDupEnd2EndSpec extends FlatSpec with Matchers with BeforeAndAfterAll {
     appender.list.size() should be > 0
   }
 
+  // When transcal receives a "=" it will use the let tactic
   "The interpreter" should "define nodup (let command)" in {
-    val logs = appender.list.asScala
     val letIndexes = logs.zipWithIndex.filter(_._1.getFormattedMessage.trim == "let").map(_._2)
     letIndexes should not be empty
   }
 
+  // When transcal receives a "->" with number to the right it will locate the term to left
   it should "locate the main term (locate commnad)" in {
-    val logs = appender.list.asScala
     val locateIndexes = logs.zipWithIndex.filter(_._1.getFormattedMessage.trim == "locate").map(_._2)
     locateIndexes should not be empty
     val locateInput = getInput(logs, locateIndexes.head)
     val locateOutput = getOutput(logs, locateIndexes.head)
     getVars(locateInput.getFormattedMessage).length should be (2)
     locateOutput.length should be (1)
+    locateOutput.head.getFormattedMessage.trim shouldEqual "¬(elem x xs) ∧ nodup xs"
   }
 
-  override protected def beforeAll(): Unit = {
-    Interpreter.main(Array(Interpreter.getClass.getResource("/examples/NoDup.tc").getPath))
+  // When transcal receives a "->" with ? to the right define the new function
+  it should "generalize the located term into a new function (generalize commnad)" in {
+    val genIndexes = logs.zipWithIndex.filter(_._1.getFormattedMessage.trim.startsWith("generalize")).map(_._2)
+    genIndexes should not be empty
+    // We will have only the new function name as a var bgecause x/xs are defined
+    val genInput = getInput(logs, genIndexes.head)
+    val genOutput = getOutput(logs, genIndexes.head)
+    getVars(genInput.getFormattedMessage).length should be (1)
+    getVars(genInput.getFormattedMessage).head shouldEqual "nodup'"
+    genOutput.length should be (2)
+    // get message with param replacement and remove as at print start
+    val generalizedExpression = genOutput.last.getFormattedMessage.trim.substring(2)
+    // this means nodup' is the lambda (α β ↦ (α ‖ elems β) ∧ nodup β)
+    // If this fails it is because there might be more ways to express this lambda.
+    // In case of failure we should generalize the assertion
+    generalizedExpression.trim shouldEqual "(α β ↦ (α ‖ elems β) ∧ nodup β) {x} xs"
   }
+
+  // When transcal receives a "->" with a number to the left and a pattern to the right
+  // it will elaborate the expression
+  it should "transform term into the requested pattern (elaborate commnad)" in {
+    val elabIndexes = logs.zipWithIndex.filter(_._1.getFormattedMessage.trim.startsWith("elaborate")).map(_._2)
+    elabIndexes should not be empty
+    // We will have only the new function name as a var bgecause x/xs are defined
+    val elabInput = getInput(logs, elabIndexes.head)
+    val elabOutput = getOutput(logs, elabIndexes.head)
+    // The pattern has 4 holes so the requested term has 4 vars
+    getVars(elabInput.getFormattedMessage).length should be (4)
+    elabOutput.length should be > 0
+    // TODO: finish test once we have output
+  }
+
+  // TODO: finish rest of app;ied tactics
 }
 
 object NoDupEnd2EndSpec {
