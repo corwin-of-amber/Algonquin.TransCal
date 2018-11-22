ec38a822bad81940f0e2585836d538235d1c3466
Tomer Ghelber <tomergelber@gmail.com>
2018-11-22 11:23:17 +0200

    Using HyperEdge class

--- src/main/scala/structures/HyperGraphManyWithOrderToOneLike.scala
+++ src/main/scala/structures/HyperGraphManyWithOrderToOneLike.scala
@@ -1,5 +1,6 @@
 package structures
 
+import structures.HyperGraphManyWithOrderToOneLike.HyperEdge
 import structures.immutable.Item
 
 /**
@@ -8,9 +9,9 @@ import structures.immutable.Item
   */
 trait HyperGraphManyWithOrderToOneLike[Node, EdgeType, +This <: HyperGraphManyWithOrderToOneLike[Node, EdgeType, This]] {
 
-  def findEdges(edge: EdgeType): Set[(Node, EdgeType, Seq[Node])]
+  def findEdges(edge: EdgeType): Set[HyperEdge[Node, EdgeType]]
 
-  def find[Id](pattern: (Item[Node, Id], Item[EdgeType, Id], Seq[Item[Node, Id]])): Set[(Node, EdgeType, Seq[Node])]
+  def find[Id](pattern: (Item[Node, Id], Item[EdgeType, Id], Seq[Item[Node, Id]])): Set[HyperEdge[Node, EdgeType]]
 
   def cycles: Boolean
 
@@ -18,6 +19,9 @@ trait HyperGraphManyWithOrderToOneLike[Node, EdgeType, +This <: HyperGraphManyWi
 
   def edges: Set[EdgeType]
 
+  def addEdge(hyperEdge: HyperEdge[Node, EdgeType]): This = {
+    addEdge(hyperEdge.target, hyperEdge.edgeType, hyperEdge.sources)
+  }
   def addEdge(hyperEdge: (Node, EdgeType, Seq[Node])): This = {
     addEdge(hyperEdge._1, hyperEdge._2, hyperEdge._3)
   }
@@ -30,4 +34,11 @@ trait HyperGraphManyWithOrderToOneLike[Node, EdgeType, +This <: HyperGraphManyWi
   def removeEdge(target: Node, edge: EdgeType, sources: Seq[Node]): This
 
   def mergeNodes(keep: Node, change: Node): This
+}
+
+object HyperGraphManyWithOrderToOneLike {
+  class HyperEdge[Node, EdgeType](val target: Node, val edgeType: EdgeType, val sources:Seq[Node])
+  object HyperEdge {
+    def apply[Node, EdgeType] (target: Node, edgeType: EdgeType, sources: Seq[Node]): HyperEdge[Node, EdgeType] = new HyperEdge(target, edgeType, sources)
+  }
 }
