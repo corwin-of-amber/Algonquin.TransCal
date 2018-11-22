ea1cf5a3c8a16e46604009f6bd8ba361a70288dd
Tomer Ghelber <tomergelber@gmail.com>
2018-11-22 11:55:40 +0200

    Added unapply for HyperEdge

--- src/main/scala/structures/HyperGraphManyWithOrderToOneLike.scala
+++ src/main/scala/structures/HyperGraphManyWithOrderToOneLike.scala
@@ -40,5 +40,6 @@ object HyperGraphManyWithOrderToOneLike {
   class HyperEdge[Node, EdgeType](val target: Node, val edgeType: EdgeType, val sources:Seq[Node])
   object HyperEdge {
     def apply[Node, EdgeType] (target: Node, edgeType: EdgeType, sources: Seq[Node]): HyperEdge[Node, EdgeType] = new HyperEdge(target, edgeType, sources)
+    def unapply[Node, EdgeType](hyperEdge: HyperEdge[Node, EdgeType]): Option[(Node, EdgeType, Seq[Node])] = Some(hyperEdge.target, hyperEdge.edgeType, hyperEdge.sources)
   }
 }
