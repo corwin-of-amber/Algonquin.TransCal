f47dad88b22777fe70c5be14111a4f8176092a1b
Tomer Ghelber <tomergelber@gmail.com>
2018-11-24 20:42:44 +0200

    Added Programs - a wrapper for the hyper graph

---
+++ src/main/scala/synthesis/Programs.scala
@@ -0,0 +1,13 @@
+package synthesis
+
+import structures.HyperGraphManyWithOrderToOne
+
+/**
+  * @author tomer
+  * @since 11/19/18
+  */
+class Programs(var hyperGraph: HyperGraphManyWithOrderToOne[Term, Term]) {
+  def reconstruct: Unit = {
+
+  }
+}
---
+++ src/main/scala/synthesis/Term.scala
@@ -0,0 +1,7 @@
+package synthesis
+
+/**
+  * @author tomer
+  * @since 11/16/18
+  */
+class Term(val id: Int)
