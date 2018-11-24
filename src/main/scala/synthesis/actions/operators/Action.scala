f35b46f847fc8a5702df851f7275dc99b382d518
Tomer Ghelber <tomergelber@gmail.com>
2018-11-24 20:40:11 +0200

    Defined Action

---
+++ src/main/scala/synthesis/actions/operators/Action.scala
@@ -0,0 +1,10 @@
+package synthesis.actions.operators
+
+import synthesis.actions.ActionSearchState
+import synthesis.search.Operator
+
+/**
+  * @author tomer
+  * @since 11/18/18
+  */
+trait Action extends Operator[ActionSearchState]
