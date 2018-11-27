ba3c32b4280121b5c2e0cb6719b6bf3d5c8301e4
Tomer Ghelber <tomergelber@gmail.com>
2018-11-27 11:47:53 +0200

    Decided on a signature for reconstruct

--- src/main/scala/synthesis/Programs.scala
+++ src/main/scala/synthesis/Programs.scala
@@ -1,13 +1,14 @@
 package synthesis
 
 import structures.HyperGraphManyWithOrderToOne
+import syntax.Tree
 
 /**
   * @author tomer
   * @since 11/19/18
   */
 class Programs(var hyperGraph: HyperGraphManyWithOrderToOne[HyperTerm, HyperTerm]) {
-  def reconstruct: Unit = {
-
+  def reconstruct(hyperTerm: HyperTerm): Iterator[Tree[Int]] = {
+    null
   }
 }
