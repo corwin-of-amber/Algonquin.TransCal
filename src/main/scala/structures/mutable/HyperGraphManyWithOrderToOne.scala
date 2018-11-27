aa0ca25fd215e0be96f51749a3987a7cfeda4eb1
Tomer Ghelber <tomergelber@gmail.com>
2018-11-27 11:34:00 +0200

    Added specific traits to mutable package

---
+++ src/main/scala/structures/mutable/HyperGraphManyWithOrderToOne.scala
@@ -0,0 +1,11 @@
+package structures.mutable
+
+import structures.HyperGraphManyWithOrderToOneLike
+
+/**
+  * @author tomer
+  * @since 11/15/18
+  */
+trait HyperGraphManyWithOrderToOne[Node, EdgeType] extends structures.HyperGraphManyWithOrderToOne[Node, EdgeType]
+  with HyperGraphManyWithOrderToOneLike[Node, EdgeType, HyperGraphManyWithOrderToOne[Node, EdgeType]] {
+}
