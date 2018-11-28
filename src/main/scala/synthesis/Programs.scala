374d3191828f277a068a18b3f4632642ad2221a6
Tomer Ghelber <tomergelber@gmail.com>
2018-11-28 13:21:53 +0200

    Implemented reconstruct with CombineSeq

--- src/main/scala/synthesis/Programs.scala
+++ src/main/scala/synthesis/Programs.scala
@@ -1,6 +1,7 @@
 package synthesis
 
 import structures.HyperGraphManyWithOrderToOne
+import structures.mutable.CombineSeq
 import syntax.Tree
 
 /**
@@ -8,7 +9,26 @@ import syntax.Tree
   * @since 11/19/18
   */
 class Programs(var hyperGraph: HyperGraphManyWithOrderToOne[HyperTerm, HyperTerm]) {
+
+
+  /* --- Public --- */
+
+  /** Builds trees from of programs where the hyper term is the base program.
+    *
+    * @param hyperTerm The hyper term to build.
+    * @return All the trees.
+    */
   def reconstruct(hyperTerm: HyperTerm): Iterator[Tree[Int]] = {
-    null
+      val targetToEdges = hyperGraph.edges.groupBy(edge=>edge.target)
+      def recursive(currentHyperTerm: HyperTerm): Iterator[Tree[Int]] = {
+        if (targetToEdges.contains(currentHyperTerm)) {
+          targetToEdges(currentHyperTerm).toIterator.flatMap(edge => {
+            new CombineSeq(edge.sources.map(recursive)).map(subtrees => new Tree[Int](edge.edgeType.id, subtrees.toList))
+          })
+        } else {
+          Iterator(new Tree[Int](currentHyperTerm.id))
+        }
+      }
+      recursive(hyperTerm)
   }
 }
