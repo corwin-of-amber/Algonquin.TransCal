825815d21ed5d1680d16a710a6d5be4e1d260daa
Tomer Ghelber <tomergelber@gmail.com>
2018-11-29 16:09:31 +0200

    Added tostring

--- src/main/scala/structures/HyperEdge.scala
+++ src/main/scala/structures/HyperEdge.scala
@@ -4,7 +4,9 @@ package structures
   * @author tomer
   * @since 11/22/18
   */
-class HyperEdge[Node, EdgeType](val target: Node, val edgeType: EdgeType, val sources:Seq[Node])
+class HyperEdge[Node, EdgeType](val target: Node, val edgeType: EdgeType, val sources:Seq[Node]) {
+  override def toString: String = f"HyperEdge($target, $edgeType, $sources)"
+}
 object HyperEdge {
   def apply[Node, EdgeType] (target: Node, edgeType: EdgeType, sources: Seq[Node]): HyperEdge[Node, EdgeType] = new HyperEdge(target, edgeType, sources)
   def unapply[Node, EdgeType](hyperEdge: HyperEdge[Node, EdgeType]): Option[(Node, EdgeType, Seq[Node])] = Some(hyperEdge.target, hyperEdge.edgeType, hyperEdge.sources)
