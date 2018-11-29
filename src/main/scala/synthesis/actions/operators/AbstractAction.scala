1057f94dc8e6e14d04b40dbb1829f444eeb6fb45
Tomer Ghelber <tomergelber@gmail.com>
2018-11-29 17:47:20 +0200

    AbstractAction pass the required result of rewrite to inner apply.

---
+++ src/main/scala/synthesis/actions/operators/AbstractAction.scala
@@ -0,0 +1,33 @@
+package synthesis.actions.operators
+
+import synthesis.actions.ActionSearchState
+import synthesis.rewrites.{RewriteSearchSpace, RewriteSearchState}
+import synthesis.search.BreadthFirstSearch
+
+/** Abstract action.
+  * @author tomer
+  * @since 11/18/18
+  */
+abstract class AbstractAction[R] extends Action {
+
+
+  /* --- Action Impl. --- */
+
+  override def apply(state: ActionSearchState): ActionSearchState = {
+    val rewriteSearch = new BreadthFirstSearch[RewriteSearchState, RewriteSearchSpace]
+    val initialState = new RewriteSearchState(state.programs.hyperGraph)
+    val spaceSearch = new RewriteSearchSpace(state.rewriteRules.toSeq, initialState, null)
+    innerApplyWithRewriter(state, rewriteSearch.search(spaceSearch))
+  }
+
+
+  /* --- Protected --- */
+
+  /** Apply after rewriter results.
+    * @param state The current state
+    * @param rewriterResult The last rewrites result
+    * @return The next action state
+    */
+  protected abstract def innerApplyWithRewriter(state: ActionSearchState, rewriterResult: Option[RewriteSearchState]): ActionSearchState
+
+}
