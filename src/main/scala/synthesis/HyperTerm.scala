c4aaa43ed86d83af93f778ffc86c225f72ea1dfb
Tomer Ghelber <tomergelber@gmail.com>
2018-11-29 16:23:21 +0200

    Converted classes to case classes for equality check. now tests are passing.

--- src/main/scala/synthesis/HyperTerm.scala
+++ src/main/scala/synthesis/HyperTerm.scala
@@ -4,6 +4,4 @@ package synthesis
   * @author tomer
   * @since 11/16/18
   */
-class HyperTerm(val id: Int) {
-  override def toString: String = f"HyperTerm($id)"
-}
+case class HyperTerm(val id: Int)
