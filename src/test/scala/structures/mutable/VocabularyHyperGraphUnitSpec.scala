c57c755285b5a5f6ce37c6a3914893a62801a7c5
Tomer Ghelber <tomergelber@gmail.com>
2018-12-04 18:34:54 +0200

    removed cycles from regular spec

--- src/test/scala/structures/mutable/VocabularyHyperGraphUnitSpec.scala
+++ src/test/scala/structures/mutable/VocabularyHyperGraphUnitSpec.scala
@@ -1,8 +1,5 @@
 package structures.mutable
 
-import org.scalacheck.{Arbitrary, Properties}
-import org.scalacheck.Prop.BooleanOperators
-import org.scalacheck.Prop.forAll
 import org.scalatest.{FlatSpec, Matchers}
 import structures.HyperGraphManyWithOrderToOneLike.HyperEdge
 
@@ -46,41 +43,5 @@ class VocabularyHyperGraphUnitSpec extends FlatSpec with Matchers {
 
     hyperGraph.nodes shouldBe Set(newTarget)
     hyperGraph.edgeTypes shouldBe Set(edgeType)
-  }
-
-  it should "has no cycle when empty" in {
-    val hyperGraph = VocabularyHyperGraph.empty[Int, Int]
-
-    hyperGraph.cycles shouldBe false
-  }
-
-  it should "has cycle" in {
-    val hyperGraph = VocabularyHyperGraph.empty[Int, Char]
-
-    val node1 = 1
-    val node2= 2
-    val edgeType1 = 'a'
-    val edgeType2 = 'b'
-
-    hyperGraph addEdge HyperEdge(node1, edgeType1, Seq(node2))
-    hyperGraph addEdge HyperEdge(node2, edgeType2, Seq(node1))
-
-    hyperGraph.cycles shouldBe true
-  }
-
-  it should "has a complicated cycle" in {
-    val hyperGraph = VocabularyHyperGraph.empty[Int, Char]
-
-    val node1 = 1
-    val node2 = 2
-    val node3 = 3
-    val node4 = 4
-    val edgeType = 'a'
-
-    hyperGraph addEdge HyperEdge(node1, edgeType, Seq(node2, node4))
-    hyperGraph addEdge HyperEdge(node2, edgeType, Seq(node3, node4))
-    hyperGraph addEdge HyperEdge(node3, edgeType, Seq(node1, node4))
-
-    hyperGraph.cycles shouldBe true
   }
 }
