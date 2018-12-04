16d5c9e6bcc021073acd1be693a76253c84ce02b
Tomer Ghelber <tomergelber@gmail.com>
2018-12-04 20:03:49 +0200

    New tests (taking no state in consideration) pass

--- src/test/scala/structures/immutable/TriePropSpec.scala
+++ src/test/scala/structures/immutable/TriePropSpec.scala
@@ -4,7 +4,6 @@ import org.scalacheck.Arbitrary
 import org.scalacheck.Prop.{BooleanOperators, forAll}
 import org.scalatest.PropSpec
 import org.scalatest.prop.Checkers
-import structures.immutable.Trie
 
 import scala.util.Random
 
@@ -39,25 +38,25 @@ class TriePropSpec extends PropSpec with Checkers {
 
   property("edges finds all that were added twice") {
     check(forAll { word: Seq[Int] =>
-      val trie = new Trie[Int]()
-      trie.add(word)
-      trie.add(word)
-      trie.words.count(_ == word) == 1
+      Trie.empty[Int].add(word).add(word).words.count(_ == word) == 1
     })
   }
 
   property("edges finds all that were added") {
     check(forAll { words: Set[Seq[Int]] =>
-      val trie = new Trie[Int]()
-      for (word <- words) trie.add(word)
-      words.intersect(trie.words).size == words.size
+      words.intersect(words.foldLeft(Trie.empty[Int])((trie, word) => trie.add(word)).words).size == words.size
     })
   }
 
   property("add than remove than add") {
     check(forAll { (trie: Trie[Int], word: Seq[Int]) =>
-      !trie.words.contains(word) ==> (trie.words.size + 1 == trie.add(word).words.size &&
-        trie.words.size-1 == trie.remove(word).words.size && trie.words.size + 1 == trie.add(word).words.size)
+      !trie.words.contains(word) ==> {
+        val trieAdd = trie.add(word)
+        trie.words.size + 1 == trieAdd.words.size && {
+          val trieRemove = trie.remove(word)
+          trieAdd.words.size-1 == trieRemove.words.size && {trieRemove.words.size + 1 == trie.add(word).words.size}
+        }
+      }
     })
   }
 
@@ -65,7 +64,7 @@ class TriePropSpec extends PropSpec with Checkers {
     def validate(trie: Trie[Int], keepLetter: Int, changeLetter: Int) = {
       val beforeLettersSize = trie.letters.size
       val newTrie = trie.replace(keepLetter, changeLetter)
-      beforeLettersSize == newTrie.letters.size + 1 && trie == newTrie
+      beforeLettersSize == newTrie.letters.size + 1 && trie != newTrie
     }
     check(forAll { trie: Trie[Int] =>
       (trie.letters.size > 1) ==> validate(trie, trie.letters.head, trie.letters.last)
@@ -77,7 +76,7 @@ class TriePropSpec extends PropSpec with Checkers {
       val beforeWordsSize = trie.words.size
       val beforeLettersSize = trie.letters.size
       val newTrie = trie.replace(keepLetter, changeLetter)
-      beforeLettersSize == newTrie.letters.size && beforeWordsSize == newTrie.words.size && trie == newTrie
+      beforeLettersSize == newTrie.letters.size && beforeWordsSize == newTrie.words.size && trie != newTrie
     }
     check(forAll { trie: Trie[Int] =>
       trie.letters.nonEmpty ==> validate(trie, trie.letters.max + 1, trie.letters.last)
@@ -89,7 +88,7 @@ class TriePropSpec extends PropSpec with Checkers {
       val beforeWordsSize = trie.words.size
       val beforeLettersSize = trie.letters.size
       val newTrie = trie.replace(keepLetter, changeLetter)
-      beforeLettersSize == newTrie.letters.size && beforeWordsSize == trie.words.size && trie == newTrie
+      beforeLettersSize == newTrie.letters.size && beforeWordsSize == trie.words.size && trie != newTrie
     }
     check(forAll { (trie: Trie[Int], keepLetter: Int) =>
       validate(trie, keepLetter, if (trie.letters.isEmpty) 1 else trie.letters.max + 1)
--- src/test/scala/structures/immutable/TrieSpec.scala
+++ src/test/scala/structures/immutable/TrieSpec.scala
@@ -3,40 +3,37 @@ package structures.immutable
 import org.scalatest.{FlatSpec, Matchers}
 import java.security.SecureRandom
 
-import structures.immutable.Trie
 
 class TrieSpec extends FlatSpec with Matchers {
 
   val random = new SecureRandom()
 
   "Trie" should "add a word" in {
-    val vocabulary = Trie.empty[Int]
+    val first = Trie.empty[Int]
     val word = randomWord
 
-    vocabulary add word
+    val vocabulary = first add word
 
     vocabulary.words should have size 1
     vocabulary.words should contain (word)
   }
 
   it should "add a word once" in {
-    val vocabulary = Trie.empty[Int]
+    val first = Trie.empty[Int]
     val word = randomWord
 
-    vocabulary add word
-    vocabulary add word
+    val vocabulary = first add word add word
 
     vocabulary.words should have size 1
     vocabulary.words should contain (word)
   }
 
   it should "add different words" in {
-    val vocabulary = Trie.empty[Int]
+    val first = Trie.empty[Int]
     val word1 = randomWord
     val word2 = randomWord
 
-    vocabulary add word1
-    vocabulary add word2
+    val vocabulary = first add word1 add word2
 
     vocabulary.words should have size 2
     vocabulary.words should contain (word1)
@@ -44,78 +41,71 @@ class TrieSpec extends FlatSpec with Matchers {
   }
 
   it should "remove only the word" in {
-    val vocabulary = Trie.empty[Int]
+    val first = Trie.empty[Int]
     val word = randomWord
 
-    vocabulary add word
-    vocabulary remove word
+    val vocabulary = first add word remove word
 
     vocabulary.words shouldBe empty
   }
 
   it should "remove only the removed word" in {
-    val vocabulary = Trie.empty[Int]
+    val first = Trie.empty[Int]
     val word1 = randomWord
     val word2 = word1 ++ randomWord
 
-    vocabulary add word1
-    vocabulary add word2
-    vocabulary remove word1
+    val vocabulary = first add word1 add word2 remove word1
 
     vocabulary.words should have size 1
     vocabulary.words should contain (word2)
   }
 
   it should "change the letter" in {
-    val vocabulary = Trie.empty[Int]
+    val first = Trie.empty[Int]
     val keep = random.nextInt
     val change = random.nextInt
     val word = Seq(change)
     val expected = Seq(keep)
 
-    vocabulary add word
-    vocabulary replace (keep, change)
+    val vocabulary = first add word replace (keep, change)
 
     vocabulary.words should have size 1
     vocabulary.words should contain (expected)
   }
 
   it should "change only the letter" in {
-    val vocabulary = Trie.empty[Int]
+    val first = Trie.empty[Int]
     val keep = random.nextInt
     val change = random.nextInt
     val otherSuffix = randomWord
     val word = change +: otherSuffix
     val expected = keep +: otherSuffix
 
-    vocabulary add word
-    vocabulary replace (keep, change)
+    val vocabulary = first add word replace (keep, change)
 
     vocabulary.words should have size 1
     vocabulary.words should contain (expected)
   }
 
   it should "change only the letter and merge new words" in {
-    val vocabulary = Trie.empty[Int]
+    val first = Trie.empty[Int]
     val keep = random.nextInt
     val change = random.nextInt
     val otherSuffix = randomWord
     val word = change +: otherSuffix
     val expected = keep +: otherSuffix
 
-    vocabulary add word
-    vocabulary add expected
-    vocabulary replace (keep, change)
+    val vocabulary = first add word add expected replace (keep, change)
 
     vocabulary.words should have size 1
     vocabulary.words should contain (expected)
   }
 
   it should "return all when empty find prefix" in {
-    val vocabulary = Trie.empty[Int]
+    var vocabulary = Trie.empty[Int]
 
     for (i <- 0 to random.nextInt(MAX_WORD_SIZE)) {
-      vocabulary.add(randomWord)
+      vocabulary = vocabulary.add(randomWord)
     }
 
     vocabulary.findByPrefix(Seq.empty) shouldBe vocabulary.words
