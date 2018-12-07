1210323221b4a55415bd3d41cca8e347a22e515f
Tomer Ghelber <tomergelber@gmail.com>
2018-12-07 16:37:58 +0200

    pointing empty access

--- src/main/scala/structures/immutable/HyperGraphManyWithOrderToOne.scala
+++ src/main/scala/structures/immutable/HyperGraphManyWithOrderToOne.scala
@@ -7,5 +7,8 @@ import structures.HyperGraphManyWithOrderToOneLike
   * @since 11/15/18
   */
 trait HyperGraphManyWithOrderToOne[Node, EdgeType] extends structures.HyperGraphManyWithOrderToOne[Node, EdgeType]
-  with HyperGraphManyWithOrderToOneLike[Node, EdgeType, HyperGraphManyWithOrderToOne[Node, EdgeType]] {
+  with HyperGraphManyWithOrderToOneLike[Node, EdgeType, HyperGraphManyWithOrderToOne[Node, EdgeType]]
+
+object HyperGraphManyWithOrderToOne {
+  def empty[Node, EdgeType]: HyperGraphManyWithOrderToOne[Node, EdgeType] = structures.immutable.VocabularyHyperGraph.empty
 }
