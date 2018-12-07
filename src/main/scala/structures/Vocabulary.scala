6df4d249925b0b32d38dbd725c44c1196a997220
Tomer Ghelber <tomergelber@gmail.com>
2018-12-07 16:30:22 +0200

    Deleted to many line

--- src/main/scala/structures/Vocabulary.scala
+++ src/main/scala/structures/Vocabulary.scala
@@ -4,5 +4,4 @@ package structures
   * @author tomer
   * @since 11/15/18
   */
-trait Vocabulary[Letter] extends VocabularyLike[Letter, Vocabulary[Letter]] {
-}
+trait Vocabulary[Letter] extends VocabularyLike[Letter, Vocabulary[Letter]]
