e17a65b0d218f85373b7d11fdedb646a3c0d33b9
Tomer Ghelber <tomergelber@gmail.com>
2018-12-07 16:36:34 +0200

    Pointing to chosen implementation

--- src/main/scala/structures/immutable/Vocabulary.scala
+++ src/main/scala/structures/immutable/Vocabulary.scala
@@ -6,5 +6,8 @@ import structures.VocabularyLike
   * @author tomer
   * @since 11/15/18
   */
-trait Vocabulary[Letter] extends structures.Vocabulary[Letter] with VocabularyLike[Letter, Vocabulary[Letter]] {
+trait Vocabulary[Letter] extends structures.Vocabulary[Letter] with VocabularyLike[Letter, Vocabulary[Letter]]
+
+object Vocabulary {
+  def empty[Letter]: Vocabulary[Letter] = Trie.empty
 }
