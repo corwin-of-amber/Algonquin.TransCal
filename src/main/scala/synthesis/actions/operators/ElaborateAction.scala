b655ab462759378313d0a335ddca2cc054e64732
Tomer Ghelber <tomergelber@gmail.com>
2018-12-16 14:27:29 +0200

    moved pickFirst inline

--- src/main/scala/synthesis/actions/operators/ElaborateAction.scala
+++ src/main/scala/synthesis/actions/operators/ElaborateAction.scala
@@ -1,13 +1,12 @@
 package synthesis.actions.operators
 
-import relentless.rewriting.{RevisionDiff, Rules}
 import relentless.rewriting.RuleBasedTactic.{Markers, ⇢}
-import syntax.AstSugar.{TI, Term}
-import syntax.Scheme
-import synthesis.{HyperTerm, HyperTermIdentifier, Programs}
+import relentless.rewriting.{RevisionDiff, Rules}
+import syntax.{Identifier, Scheme, Tree}
+import synthesis.HyperTermIdentifier
 import synthesis.actions.ActionSearchState
 
-/**
+/** IS THIS REALLY NEEDED IN CASE WE KEEP PROGRAMS?
   * @author tomer
   * @since 11/18/18
   */
@@ -16,18 +15,14 @@ class ElaborateAction(goalScheme: Scheme) extends Action {
 
     val work = state.programs
 
-    work.hyperGraph.findEdges(HyperTermIdentifier(Markers.goal.leaf)).headOption match {
-      case Some(m) =>
-        val elaborated = goalScheme(pickFirst(m.edgeType, work).subtrees)
-        val original = work.reconstruct(m(1)).toStream.headOption getOrElse TI("?")
-        val t = (RevisionDiff(List(), List(), List(original ⇢ elaborated)), Rules.empty)
+    state.programs.hyperGraph.findEdges(HyperTermIdentifier(Markers.goal.leaf)).headOption match {
+      case Some(hyperEdge) =>
+        val elaborated = state.programs.reconstruct(hyperEdge.edgeType).map(goalScheme(_)).next()
+        val original = state.programs.reconstruct(hyperEdge.target).toStream.headOption getOrElse new Tree[Identifier](new Identifier("?"))
+        val t = (RevisionDiff(List(), List(), List(original ⇢ elaborated)), Rules.empty)  // Equal for adding two terms to the same hyper term
         new ActionSearchState(null, state.rewriteRules)
       case None => state
     }
-  }
-
-  def pickFirst(match_ : HyperTerm, trie: Programs): Term = {
-    trie.reconstruct(match_).next()
   }
 }
 
