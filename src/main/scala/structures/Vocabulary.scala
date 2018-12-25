2db6d235191d633903c7e2651cb4217043fe5676
Tomer Ghelber <tomergelber@gmail.com>
2018-12-25 10:27:53 +0200

    Removed references

--- src/main/scala/structures/Vocabulary.scala
+++ src/main/scala/structures/Vocabulary.scala
@@ -5,17 +5,3 @@ package structures
   * @since 11/15/18
   */
 trait Vocabulary[Letter] extends VocabularyLike[Letter, Vocabulary[Letter]]
-
-object Vocabulary {
-  // Reference VocabularyLike
-  type Word[Letter] = VocabularyLike.Word[Letter]
-  type Item[Value, Id] = VocabularyLike.Item[Value, Id]
-  type Hole[Value, Id] = VocabularyLike.Hole[Value, Id]
-  val Hole: VocabularyLike.Hole.type = VocabularyLike.Hole
-  type Explicit[Value, Id] = VocabularyLike.Explicit[Value, Id]
-  val Explicit: VocabularyLike.Explicit.type = VocabularyLike.Explicit
-  type Ignored[Value, Id] = VocabularyLike.Ignored[Value, Id]
-  val Ignored: VocabularyLike.Ignored.type = VocabularyLike.Ignored
-  type LetterPattern[Letter, Id] = VocabularyLike.LetterPattern[Letter, Id]
-  type WordPattern[Letter, Id] = VocabularyLike.WordPattern[Letter, Id]
-}
--- src/main/scala/structures/immutable/Vocabulary.scala
+++ src/main/scala/structures/immutable/Vocabulary.scala
@@ -1,6 +1,7 @@
 package structures.immutable
 
 import structures.VocabularyLike
+import structures.VocabularyLike.Word
 
 /**
   * @author tomer
@@ -12,16 +13,4 @@ object Vocabulary {
   def empty[Letter]: Vocabulary[Letter] = Trie.empty
 
   def apply[Letter](words: Set[Word[Letter]]): Vocabulary[Letter] = Trie(words)
-
-  // Reference structures.Vocabulary
-  type Word[Letter] = structures.Vocabulary.Word[Letter]
-  type Item[Value, Id] = structures.Vocabulary.Item[Value, Id]
-  type Hole[Value, Id] = structures.Vocabulary.Hole[Value, Id]
-  val Hole: structures.Vocabulary.Hole.type = structures.Vocabulary.Hole
-  type Explicit[Value, Id] = structures.Vocabulary.Explicit[Value, Id]
-  val Explicit: structures.Vocabulary.Explicit.type = structures.Vocabulary.Explicit
-  type Ignored[Value, Id] = structures.Vocabulary.Ignored[Value, Id]
-  val Ignored: structures.Vocabulary.Ignored.type = structures.Vocabulary.Ignored
-  type LetterPattern[Letter, Id] = structures.Vocabulary.LetterPattern[Letter, Id]
-  type WordPattern[Letter, Id] = structures.Vocabulary.WordPattern[Letter, Id]
 }
