606f81ace20d9c45b7e5e1ec2d55d1ea8ac1c12d
Tomer Ghelber <tomergelber@gmail.com>
2018-12-26 13:38:44 +0200

    Using operator instead of fold

--- src/main/scala/scala/collection/mutable/HashMultiMap.scala
+++ src/main/scala/scala/collection/mutable/HashMultiMap.scala
@@ -9,7 +9,9 @@ object HashMultiMap {
 
   def empty[Key, Value]: mutable.MultiMap[Key, Value] = new mutable.HashMap[Key, mutable.Set[Value]] with mutable.MultiMap[Key, Value]
 
-  def apply[Key, Value](k2vs: collection.Map[Key, collection.Set[Value]]): mutable.MultiMap[Key, Value] = k2vs.foldLeft(empty[Key, Value])(put)
+  def apply[Key, Value](k2vs: collection.Map[Key, collection.Set[Value]]): mutable.MultiMap[Key, Value] = {
+    empty[Key, Value] ++ k2vs
+  }
 
   def put[Key, Value](multimap: mutable.MultiMap[Key, Value], kvs: (Key, collection.Set[Value])): mutable.MultiMap[Key, Value] = kvs._2.foldLeft(multimap)((multimap, value) => {multimap. addBinding(kvs._1, value)})
 }
