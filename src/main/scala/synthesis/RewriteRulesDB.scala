e07b890a274ccf6d92aaa140da970be4824ebab5
Tomer Ghelber <tomergelber@gmail.com>
2018-12-30 11:11:47 +0200

    Copied all the DB from old build

--- src/main/scala/synthesis/RewriteRulesDB.scala
+++ src/main/scala/synthesis/RewriteRulesDB.scala
@@ -1,8 +1,9 @@
 package synthesis
 
-import relentless.BasicSignature
+import relentless.BasicSignature._
+import relentless.rewriting.RewriteRule._
 import structures.immutable.HyperGraphManyWithOrderToOne
-import structures.{EmptyMetadata, HyperEdge}
+import structures.{EmptyMetadata, HyperEdge, Metadata}
 import syntax.AstSugar._
 import syntax.{AstSugar, Identifier}
 import synthesis.rewrites.RewriteRule
@@ -16,6 +17,7 @@ import synthesis.rewrites.Template.{ExplicitTerm, ReferenceTerm, TemplateTerm}
 trait RewriteRulesDB {
   protected def vars: Set[Identifier]
   protected def ruleTemplates: Seq[Term]
+  protected def metadata: Metadata
   lazy val rewriteRules: Seq[RewriteRule] = ruleTemplates.flatMap(ruleTemplatesToRewriteRules)
 
   private def ruleTemplatesToRewriteRules(ruleTemplate: Term): Set[RewriteRule] = {
@@ -39,8 +41,8 @@ trait RewriteRulesDB {
         val leftPattern = termToHyperPattern(leftTerm)
         val rightPattern = termToHyperPattern(rightTerm)
         Set(
-          new RewriteRule(leftPattern, rightPattern, (a, b) => EmptyMetadata),
-          new RewriteRule(rightPattern, leftPattern, (a, b) => EmptyMetadata)
+          new RewriteRule(leftPattern, rightPattern, (a, b) => metadata),
+          new RewriteRule(rightPattern, leftPattern, (a, b) => metadata)
         )
       case relentless.rewriting.RewriteRule.`=>` =>
         val leftTerm = ruleTemplate.subtrees.head
@@ -48,7 +50,7 @@ trait RewriteRulesDB {
         println(s"directional rewrite $leftTerm $rightTerm")
         val conditions = termToHyperPattern(leftTerm)
         val destination = termToHyperPattern(rightTerm)
-        Set(new RewriteRule(conditions, destination, (a, b) => EmptyMetadata))
+        Set(new RewriteRule(conditions, destination, (a, b) => metadata))
       case _ =>
         println("unknown")
         Set()
@@ -57,12 +59,85 @@ trait RewriteRulesDB {
 }
 
 object SimpleRewriteRulesDB extends RewriteRulesDB {
-  override protected val vars: Set[Identifier] = Set()
+  override protected val vars: Set[Identifier] = Set(x, y, z, `x'`, xs).map(_.root)
 
   override protected val ruleTemplates: Seq[Term] = Seq(
-    ~ BasicSignature.tt =:= BasicSignature.ff,
-    ~ BasicSignature.ff =:= BasicSignature.tt
+    `⇒:`(tt, y) =:> id(y),
+    `⇒:`(ff, y) =:> ff,
+    ~tt =:= ff,
+    ~ff =:= tt,
+    (x /: ff) =:> id(x),
+    (ff /: x) =:> id(x),
+    id(id(x)) =:> id(x),
+
+    (x =:= `x'`) =:= in(`x'`, `{}`(x)),
+    elem(x, cons(`x'`, `xs'`)) =:= ((x =:= `x'`) | elem(x, `xs'`)),
+    ~(x =:= y) =:= `!=:=`(x, y),
+    ~in(x, y) =:= not_in(x, y),
+    not_in(x, xs) =:= set_disj(`{}`(x), xs),
+    set_disj(xs, `{}`(x)) =:> not_in(x, xs),
+    ~(x | y) =:= (~x & ~y),
+    ~(x & y) =:= (~x | ~y),
+    (set_disj(x, xs) & set_disj(y, xs)) =:= set_disj(set_union(x, y), xs),
+    (set_disj(xs, x) & set_disj(xs, y)) =:= set_disj(xs, set_union(x, y)),
+    elem(x, xs) =:= in(x, elems(xs)),
+    elems(cons(`x'`, `xs'`)) =:= set_union(`{}`(`x'`), elems(`xs'`)), // <-- this one is somewhat superfluous?
+
+    snoc(y, x) =:= ++(y, cons(x, nil)),
+    ++(nil, `xs'`) =:> id(`xs'`),
+    ++(`xs'`, nil) =:> id(`xs'`),
+    ++(x, ++(y, z)) =:= ++(++(x, y), z),
+    ++(cons(x, xs), `xs'`) =:= cons(x, ++(xs, `xs'`)),
+
+    (<(x, y) ||| tt) =:> ≤(x, y),
+    ≤(x, y) ||> (min(x, y) =:> id(x)),
+    ≤(x, y) ||> (min(y, x) =:> id(x)),
+    //    min(x, y) =:> min(y,x),
+
+    ≤(x, y) ||> (bounded_minus(x, y) =:> zero),
+
+    take(xs, zero) =:> nil,
+    take(xs, len(xs)) =:> xs,
+    take(++(xs, `xs'`), x) =:> ++(take(xs, min(len(xs), x)), take(`xs'`, bounded_minus(x, len(xs)))),
+
+    // merge range
+    ++(range_exclude(x,y), range_exclude(y, z)) =:> range_exclude(x, z),
+    // exclude to include
+    range_exclude(x, y + one) =:= range_include(x, y),
+    // singleton range
+    range_include(x, x) =:= cons(x, nil),
+    (in(z, range_exclude(x, y)) ||| tt) =:> (≤(x, z) ||| <(z, y))
   )
+
+  override protected def metadata: Metadata = EmptyMetadata
+}
+
+object AssociativeRewriteRulesDB extends RewriteRulesDB {
+  override protected val vars: Set[Identifier] = Set(x, y, z).map(_.root)
+
+  override protected val ruleTemplates: Seq[Term] = Seq(
+    (x & (y & z)) =:= (x & y & z)
+  )
+
+  override protected def metadata: Metadata = AssociativeMetadata
+
+  case object AssociativeMetadata extends Metadata {
+    override def toStr: String = "AssociativeMetadata"
+  }
+}
+
+object ExistentialRewriteRulesDB extends RewriteRulesDB {
+  override protected val vars: Set[Identifier] = Set(xs, exist).map(_.root)
+
+  override protected val ruleTemplates: Seq[Term] = Seq(
+    xs =:> ++(take(xs, exist), drop(xs, exist))
+  )
+
+  override protected def metadata: Metadata = ExistentialMetadata
+
+  case object ExistentialMetadata extends Metadata {
+    override def toStr: String = "ExistentialMetadata"
+  }
 }
 
 object Main extends App {
