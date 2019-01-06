586e30c9e8523b0940792d8f9ff8545f0696a3cd
Tomer Ghelber <tomergelber@gmail.com>
2019-01-06 08:33:02 +0200

    sealed prevents from others to extend the trait

--- src/main/scala/structures/Hole.scala
+++
@@ -1,7 +0,0 @@
-package structures
-
-/**
-  * @author tomer
-  * @since 12/25/18
-  */
-case class Hole[Value, Id](id: Id) extends Item[Value, Id]
--- src/main/scala/structures/Item.scala
+++ src/main/scala/structures/Item.scala
@@ -4,4 +4,7 @@ package structures
   * @author tomer
   * @since 12/25/18
   */
-trait Item[Value, Id]
+sealed trait Item[Value, Id]
+case class Explicit[Value, Id](value: Value) extends Item[Value, Id]
+case class Hole[Value, Id](id: Id) extends Item[Value, Id]
+case class Ignored[Value, Id]() extends Item[Value, Id]
