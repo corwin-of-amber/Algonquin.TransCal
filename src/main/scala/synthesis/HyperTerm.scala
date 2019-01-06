0ab7ce5f92efd3acb0aef61d0b8e75520624ba34
Tomer Ghelber <tomergelber@gmail.com>
2019-01-06 16:51:18 +0200

    HyperTerm is sealed

--- src/main/scala/synthesis/HyperTerm.scala
+++ src/main/scala/synthesis/HyperTerm.scala
@@ -6,7 +6,7 @@ import syntax.Identifier
   * @author tomer
   * @since 11/16/18
   */
-trait HyperTerm
+sealed trait HyperTerm
 case class HyperTermId(id: Int) extends HyperTerm
 
 case class HyperTermIdentifier(identifier: Identifier) extends HyperTerm
