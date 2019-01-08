a54ad8ecc8eaf152b24aca663d8fe49707eeeb1c
eytan.s <eytan.singher@gmail.com>
2019-01-08 12:33:25 +0200

    Tests: Changed tree factory to allow trees of depth up to 5

--- src/test/scala/synthesis/synthesis.scala
+++ src/test/scala/synthesis/synthesis.scala
@@ -6,11 +6,21 @@ import syntax.{Identifier, Tree}
 import scala.util.Random
 
 package object synthesis {
-  def TreeGenFactory[Node](valuesSource: Gen[Node]): Gen[Tree[Node]] = for {
-    root <- valuesSource
-    childrenSize = Random.nextInt(6)
-    children <- listOfN(childrenSize, valuesSource.map(new Tree(_)))
-  } yield new Tree[Node](root, children)
+  def TreeGenFactory[Node](valuesSource: Gen[Node]): Gen[Tree[Node]] = {
+    val leafGen: Gen[Tree[Node]] = valuesSource.map(new Tree(_))
+
+    def nodeGen(depth: Int): Gen[Tree[Node]] = {
+      val treeGen = if (depth == 0) leafGen else oneOf(leafGen, nodeGen(depth - 1))
+      for {
+        root <- valuesSource
+        childrenSize = Random.nextInt(6) + 1
+        children <- listOfN(childrenSize, treeGen)
+      } yield new Tree[Node](root, children)
+    }
+
+    oneOf(leafGen, nodeGen(5))
+  }
+
 
   val identifierGen: Gen[Identifier] = oneOf((0 to 50).map(new Identifier(_)))
 
