5448d04319894155b1011052d7feb81bceef4d75
eytan.s <eytan.singher@gmail.com>
2019-01-08 13:50:55 +0200

    Tests: Fixed and finished LocateAction test

--- src/test/scala/synthesis/actions/operators/LocateActionTest.scala
+++ src/test/scala/synthesis/actions/operators/LocateActionTest.scala
@@ -7,14 +7,16 @@ import structures.immutable.{HyperGraphManyWithOrderToOne, VocabularyHyperGraph}
 import syntax.AstSugar.Term
 import syntax.Identifier
 import synthesis.actions.ActionSearchState
+import synthesis.actions.operators.LocateAction.LocateMetadata
 import synthesis.{HyperTermId, HyperTermIdentifier, Programs, SimpleRewriteRulesDB}
 import synthesis.language.OldParser
+import synthesis.rewrites.RewriteRule
 import synthesis.rewrites.Template.{ExplicitTerm, ReferenceTerm, TemplateTerm}
 
 class LocateActionTest extends FunSuite with Matchers with LazyLogging {
 
   test("can locate the whole term") {
-    val rules = SimpleRewriteRulesDB().rewriteRules
+    val rules: Set[RewriteRule] = Set.empty
     logger.info("Using these rewrite rules:")
     logger.info(rules.mkString("\n"))
     val mainTerm = (new OldParser).apply("concat = ((⟨⟩ ↦ ⟨⟩) / (?xs :: ?xss ↦ xs ++ concat xss))   [++]")
@@ -25,8 +27,27 @@ class LocateActionTest extends FunSuite with Matchers with LazyLogging {
     )
     val template = VocabularyHyperGraph.empty.addEdge(equalEdge)
     val newState = new LocateAction(HyperTermIdentifier(new Identifier("anchor", kind = Programs.Kinds.NonConstructable.toString)), template)(state)
-    val newRules = newState.rewriteRules.diff(rules)
-    newRules.size should be (1)
+    val newEdges = newState.programs.hyperGraph.edges.diff(state.programs.hyperGraph.edges)
+    newEdges.size should be (1)
+    newEdges.head.edgeType.identifier.literal should be ("anchor")
+    newEdges.head.metadata.count(_.isInstanceOf[LocateMetadata]) should be (1)
   }
 
+  test("locate one out of two") {
+    val rules: Set[RewriteRule] = Set.empty
+    logger.info("Using these rewrite rules:")
+    logger.info(rules.mkString("\n"))
+    val mainTerm = (new OldParser).apply("concat = ((⟨⟩ ↦ ⟨⟩) / (?xs :: ?xss ↦ xs ++ concat xss))   [++]")
+    val progs = Programs(mainTerm)
+    val state = new ActionSearchState(progs, rules)
+    val equalEdge = HyperEdge[TemplateTerm[HyperTermId], TemplateTerm[HyperTermIdentifier]](
+      ReferenceTerm(0), ExplicitTerm(HyperTermIdentifier(new Identifier("↦"))), Seq(ReferenceTerm(1), ReferenceTerm(2)), EmptyMetadata
+    )
+    val template = VocabularyHyperGraph.empty.addEdge(equalEdge)
+    val newState = new LocateAction(HyperTermIdentifier(new Identifier("anchor", kind = Programs.Kinds.NonConstructable.toString)), template)(state)
+    val newEdges = newState.programs.hyperGraph.edges.diff(state.programs.hyperGraph.edges)
+    newEdges.size should be (1)
+    newEdges.head.edgeType.identifier.literal should be ("anchor")
+    newEdges.head.metadata.count(_.isInstanceOf[LocateMetadata]) should be (1)
+  }
 }
