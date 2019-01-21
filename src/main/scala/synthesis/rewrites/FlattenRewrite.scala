bc289b05a43ec2e77aa7d3376b485159c19c6985
Tomer Ghelber <tomergelber@gmail.com>
2019-01-21 18:25:03 +0200

    Fixed logic

--- src/main/scala/synthesis/rewrites/FlattenRewrite.scala
+++ src/main/scala/synthesis/rewrites/FlattenRewrite.scala
@@ -2,42 +2,31 @@ package synthesis.rewrites
 
 import language.Language
 import structures._
-import structures.HyperGraphManyWithOrderToOneLike.{HyperEdgePattern, HyperGraphPrefix}
-import structures.immutable.HyperGraphManyWithOrderToOne.HyperGraphPattern
 import structures.immutable.VocabularyHyperGraph
-import syntax.Identifier
-import synthesis.{HyperTermId, HyperTermIdentifier, rewrites}
 import synthesis.rewrites.rewrites._
 import synthesis.search.Operator
+import synthesis.{HyperTermId, HyperTermIdentifier}
 
 object FlattenRewrite extends Operator[RewriteSearchState] {
-  private val outerApply: HyperEdge[Item[HyperTermId, Int], (Item[HyperTermIdentifier, Int], Boolean)] = {
-    val temp = patternEdgeCreator(Hole[HyperTermId, Int](0), Language.applyId, Seq(Hole[HyperTermId, Int](1)))
-      temp.copy(edgeType = (temp.edgeType, true))
-  }
+  private val outerApply: HyperEdge[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] =
+    patternEdgeCreator(Hole[HyperTermId, Int](0), Language.applyId, Seq(Hole[HyperTermId, Int](1), Repetition.rep0[HyperTermId, Int](Int.MaxValue, Ignored[HyperTermId, Int]()).get))
 
-  private val innerApply: HyperEdge[Item[HyperTermId, Int], (Item[HyperTermIdentifier, Int], Boolean)] = {
-    val temp = patternEdgeCreator(Hole[HyperTermId, Int](1), Language.applyId, Seq(Hole[HyperTermId, Int](2)))
-    temp.copy(edgeType = (temp.edgeType, true))
-  }
+  private val innerApply: HyperEdge[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] =
+    patternEdgeCreator(Hole[HyperTermId, Int](1), Language.applyId, Seq(Hole[HyperTermId, Int](2), Repetition.rep0[HyperTermId, Int](Int.MaxValue, Ignored[HyperTermId, Int]()).get))
 
-  private val twoAppliesSubgraph: VocabularyHyperGraph[Item[HyperTermId, Int], (Item[HyperTermIdentifier, Int], Boolean)] =
-    VocabularyHyperGraph[Item[HyperTermId, Int], (Item[HyperTermIdentifier, Int], Boolean)](Seq(outerApply, innerApply): _*)
+  private val twoAppliesSubgraph: VocabularyHyperGraph[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] =
+    VocabularyHyperGraph(Seq(outerApply, innerApply): _*)
 
   override def apply(state: RewriteSearchState): RewriteSearchState = {
-    val results = state.graph.findSubgraphPrefix[Int, HyperGraphManyWithOrderToOne[Item[HyperTermId, Int], (Item[HyperTermIdentifier, Int], Boolean)]](twoAppliesSubgraph)
-    val newEdges = for ((idMap, identMap) <- results) yield {
-      val outerP: HyperEdge[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] = patternEdgeCreator(idMap(0), Language.applyId, Seq(toExplicit(idMap(1))))
-      val edges = for (outer <- state.graph.findPrefix[Int](outerP.copy(edgeType = (outerP.edgeType, true)))) yield {
-        val innerP: HyperEdge[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] = patternEdgeCreator(idMap(1), Language.applyId, Seq(toExplicit(idMap(2))))
-        for (inner <- state.graph.findPrefix[Int](innerP.copy(edgeType = (innerP.edgeType, true)))) yield {
-          HyperEdge[HyperTermId, HyperTermIdentifier](idMap(0), HyperTermIdentifier(Language.applyId), inner.sources ++ outer.sources, EmptyMetadata)
-        }
-      }
-      edges.flatten
+    val results = state.graph.findSubgraph(twoAppliesSubgraph)
+    val newEdges = for (
+      (idMap, _) <- results;
+      outer <-state.graph.filter(e => e.target == idMap(0) && e.sources.head == idMap(1));
+      inner <- state.graph.filter(e => e.target == idMap(1) && e.sources.head == idMap(2))) yield {
+      HyperEdge[HyperTermId, HyperTermIdentifier](idMap(0), HyperTermIdentifier(Language.applyId), inner.sources ++ outer.sources.drop(1), EmptyMetadata)
     }
 
 
-    new RewriteSearchState(state.graph.addEdges(newEdges.flatten))
+    new RewriteSearchState(state.graph.addEdges(newEdges))
   }
 }
