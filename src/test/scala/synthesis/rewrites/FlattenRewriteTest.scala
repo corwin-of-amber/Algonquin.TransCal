0e117fce4089e9c226a599b217eebe215433af3d
eytan.s <eytan.singher@gmail.com>
2019-01-21 17:31:53 +0200

    Tests: fixed flatten test.

--- src/test/scala/synthesis/rewrites/FlattenRewriteTest.scala
+++ src/test/scala/synthesis/rewrites/FlattenRewriteTest.scala
@@ -7,17 +7,23 @@ import org.scalatest.prop.Checkers
 import structures.{EmptyMetadata, HyperEdge}
 import syntax.AstSugar.Term
 import syntax.{Identifier, Tree}
-import language.TranscalParser
+import language.{Language, TranscalParser}
 import language.Language._
-import synthesis.Programs
+import structures.immutable.VocabularyHyperGraph
+import synthesis.{HyperTermId, HyperTermIdentifier, Programs}
 import synthesis.rewrites.Template.ReferenceTerm
 
 
 class FlattenRewriteTest extends PropSpec with Checkers  {
 
   property("flatten works") {
-    val term = new TranscalParser().apply("(a b) c d")
-    val state = new RewriteSearchState(Programs.destruct(term))
+    val edges = Set(HyperEdge(HyperTermId(0), HyperTermIdentifier(Language.applyId), Seq(HyperTermId(1), HyperTermId(2)), EmptyMetadata),
+      HyperEdge(HyperTermId(1), HyperTermIdentifier(Language.applyId), Seq(HyperTermId(3), HyperTermId(4), HyperTermId(5)), EmptyMetadata),
+      HyperEdge(HyperTermId(2), HyperTermIdentifier(new Identifier("x")), Seq(), EmptyMetadata),
+      HyperEdge(HyperTermId(3), HyperTermIdentifier(new Identifier("f")), Seq(), EmptyMetadata),
+      HyperEdge(HyperTermId(4), HyperTermIdentifier(new Identifier("z")), Seq(), EmptyMetadata),
+      HyperEdge(HyperTermId(5), HyperTermIdentifier(new Identifier("y")), Seq(), EmptyMetadata))
+    val state = new RewriteSearchState(VocabularyHyperGraph(edges.toSeq: _*))
     val newEdge = FlattenRewrite(state).graph.edges -- state.graph.edges
     check(newEdge.size == 1)
     check(newEdge.head.sources.size == 2)
