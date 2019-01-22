28de6aeb486d5fb4709f8f77922043d771736cfd
eytan.s <eytan.singher@gmail.com>
2019-01-22 15:34:48 +0200

    Rewrites: Added function flatten. Updated tests

--- src/main/scala/synthesis/rewrites/FlattenRewrite.scala
+++ src/main/scala/synthesis/rewrites/FlattenRewrite.scala
@@ -7,26 +7,38 @@ import synthesis.rewrites.rewrites._
 import synthesis.search.Operator
 import synthesis.{HyperTermId, HyperTermIdentifier}
 
+
+/** The flatten works on 2 things. If we have a leftmost apply under an apply it means the function that we will use is
+  * under the lower apply. We should flatten the applies to a single one. Secondly, if we have an apply which has a
+  * first parameter that isn't an apply, the function that will be used, so we should flatten it into a single function.
+  */
 object FlattenRewrite extends Operator[RewriteSearchState] {
+  object FlattenMetadata extends Metadata {
+    override protected def toStr: String = "FlattenMetadata"
+  }
+
   private val outerApply: HyperEdge[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] =
     patternEdgeCreator(Hole[HyperTermId, Int](0), Language.applyId, Seq(Hole[HyperTermId, Int](1), Repetition.rep0[HyperTermId, Int](Int.MaxValue, Ignored[HyperTermId, Int]()).get))
 
-  private val innerApply: HyperEdge[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] =
-    patternEdgeCreator(Hole[HyperTermId, Int](1), Language.applyId, Seq(Hole[HyperTermId, Int](2), Repetition.rep0[HyperTermId, Int](Int.MaxValue, Ignored[HyperTermId, Int]()).get))
+  private val innerFunc: HyperEdge[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] =
+    patternEdgeCreator(Hole[HyperTermId, Int](1), Hole[HyperTermIdentifier, Int](0), Seq(Repetition.rep0[HyperTermId, Int](Int.MaxValue, Ignored[HyperTermId, Int]()).get))
 
-  private val twoAppliesSubgraph: VocabularyHyperGraph[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] =
-    VocabularyHyperGraph(Seq(outerApply, innerApply): _*)
+  private val applyFuncGraph: VocabularyHyperGraph[Item[HyperTermId, Int], Item[HyperTermIdentifier, Int]] =
+    VocabularyHyperGraph(Seq(outerApply, innerFunc): _*)
 
   override def apply(state: RewriteSearchState): RewriteSearchState = {
-    val results = state.graph.findSubgraph(twoAppliesSubgraph)
-    val newEdges = for (
-      (idMap, _) <- results;
-      outer <-state.graph.filter(e => e.target == idMap(0) && e.sources.head == idMap(1));
-      inner <- state.graph.filter(e => e.target == idMap(1) && e.sources.head == idMap(2))) yield {
-      HyperEdge[HyperTermId, HyperTermIdentifier](idMap(0), HyperTermIdentifier(Language.applyId), inner.sources ++ outer.sources.drop(1), EmptyMetadata)
+    // TODO: may need to do this for a few times so should find an efficient way
+    // TODO: Don't use filter if it is O(n)
+
+    // Change apply to function
+    val funcResults = state.graph.findSubgraph(applyFuncGraph)
+    val newFuncEdges = for (
+      (idMap, identMap) <- funcResults;
+      outer <- state.graph.filter(e => e.target == idMap(0) && e.sources.head == idMap(1));
+      inner <- state.graph.filter(e => e.target == idMap(1) && e.edgeType == identMap(0))) yield {
+      HyperEdge[HyperTermId, HyperTermIdentifier](idMap(0), identMap(0), inner.sources ++ outer.sources.drop(1), outer.metadata.merge(inner.metadata).merge(FlattenMetadata))
     }
 
-
-    new RewriteSearchState(state.graph.addEdges(newEdges))
+    new RewriteSearchState(state.graph.addEdges(newFuncEdges))
   }
 }
