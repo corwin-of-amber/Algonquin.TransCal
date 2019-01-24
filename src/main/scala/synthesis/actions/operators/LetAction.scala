eb64e602c8b827fdfd9cfc4ba8bf2fe34ff96aa9
eytan.s <eytan.singher@gmail.com>
2019-01-24 15:45:23 +0200

    Rewrites: Updated beta reduction and let code. Still doesn't compile so commented out.

--- src/main/scala/synthesis/actions/operators/LetAction.scala
+++ src/main/scala/synthesis/actions/operators/LetAction.scala
@@ -1,16 +1,17 @@
 //package synthesis.actions.operators
 //
 //import semantics.LambdaCalculus
-//import syntax.Scheme
+//import syntax.{Identifier, Scheme, Tree}
 //import synthesis.actions.ActionSearchState
 //import LambdaCalculus.↦⁺
+//import language.Language
 //import structures.HyperEdge
 //import syntax.AstSugar._
-//import synthesis.rewrites.RewriteRule
+//import synthesis.rewrites.{RewriteRule, Template}
 //import synthesis.{HyperTermId, HyperTermIdentifier}
 //import synthesis.rewrites.RewriteRule.HyperPattern
 //import synthesis.rewrites.RewriteSearchState.HyperGraph
-//import synthesis.rewrites.Template.TemplateTerm
+//import synthesis.rewrites.Template.{ExplicitTerm, ReferenceTerm, TemplateTerm}
 //
 ///** Let action adds a rewrite rule to show the equality between the two templates.
 //  * This action also adds all equalities
@@ -19,22 +20,43 @@
 //  * @author tomer
 //  * @since 11/18/18
 //  */
-//class LetAction(lhs: HyperPattern, rhs: HyperPattern, biDirectional: Boolean) extends Action {
+//class LetAction(term: Term) extends Action {
 //  // TODO: check what skolemize was
-//  // TODO: make all function definitions into lambdas so we will have correct rewriting
 //
-//  DO BETA REDUCTION
+//  // Beta reduction is done by adding rewrite rules and using flatten
 //
-//  def rules: List[RewriteRule] = {
-//    val rewrites: List[RewriteRule] = (equality ++ derivedFrom(equality)) map skolemize flatMap (RewriteRule(_, RewriteRule.Category.Definition))
-//    rewrites
+//  assert(Language.builtinDefinitions contains term.root.literal.toString)
+//
+//  // Start by naming lambdas and removing the bodies into rewrites.
+//  // I can give temporary name and later override them by using merge nodes
+//  private val updatedTerm: Term = {
+//    def renameLambdas(t: Term): (Set[RewriteRule], Term) = {
+//      val results = t.subtrees map renameLambdas
+//      val subtrees = results.map(_._2)
+//      val rewrites = results.flatMap(_._1)
+//      t.root match {
+//        case Language.lambdaId =>
+//          val newFunc = LetAction.functionNamer()
+//          val params = if(subtrees(0).root == Language.tupleId) subtrees(0).subtrees else List(subtrees(0))
+//          val conditions = Template(ReferenceTerm(0), ExplicitTerm(HyperTermIdentifier(newFunc)), params)
+//          (Set(), new Tree(newFunc))
+//        case _ => (rewrites.toSet, new Tree(t.root, subtrees))
+//      }
+//    }
+//    renameLambdas(term)
 //  }
 //
-//  val elab: List[Revision.Equivalence] = equality map (_.template) collect { case T(`=`, List(lhs, rhs)) => lhs ⇢ rhs }
-//
-//
 //  override def apply(state: ActionSearchState): ActionSearchState = {
+//
+//    // Take main expression and create a rewrite
 //    val newRules = Set(new RewriteRule(lhs, rhs, RewriteRule.))
 //    new ActionSearchState(state.programs, state.rewriteRules ++ newRules)
+//  }
+//}
+//
+//object LetAction {
+//  protected val functionNamer: () => Identifier = {
+//    val creator = Stream.from(language.Language.arity.size).iterator
+//    () => I(s"f${creator.next()}")
 //  }
 //}
