70e8a1f5608e158b9e8c9a2e18c614c523c2e7aa
eytan.s <eytan.singher@gmail.com>
2019-01-30 13:43:50 +0200

    Rewrites: Should use SearchState Operators and not RewriteRules.

--- src/main/scala/synthesis/rewrites/RewriteSearchSpace.scala
+++ src/main/scala/synthesis/rewrites/RewriteSearchSpace.scala
@@ -1,12 +1,12 @@
 package synthesis.rewrites
 
-import synthesis.search.SearchSpace
+import synthesis.search.{Operator, SearchSpace}
 
 /**
   * @author tomer
   * @since 11/18/18
   */
-class RewriteSearchSpace(rewriteRules: Seq[RewriteRule], initialState: RewriteSearchState, goalPredicate: RewriteSearchState => Boolean) extends SearchSpace[RewriteSearchState] {
+class RewriteSearchSpace(rewriteRules: Seq[Operator[RewriteSearchState]], initialState: RewriteSearchState, goalPredicate: RewriteSearchState => Boolean) extends SearchSpace[RewriteSearchState] {
   override def neighbors(state: RewriteSearchState): Stream[RewriteSearchState] =
     rewriteRules.toStream.map(rewriteRule => rewriteRule.apply(state))
 
