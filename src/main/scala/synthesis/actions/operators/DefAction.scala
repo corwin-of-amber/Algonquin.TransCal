a37f10483370f4500deadec6aa5a4c85e939d353
eytan.s <eytan.singher@gmail.com>
2019-01-30 16:48:07 +0200

    Actions: Fixed def to remove ? from var names.

--- src/main/scala/synthesis/actions/operators/DefAction.scala
+++ src/main/scala/synthesis/actions/operators/DefAction.scala
@@ -2,15 +2,21 @@ package synthesis.actions.operators
 
 import language.Language
 import syntax.AstSugar.Term
+import syntax.{Identifier, Tree}
 import synthesis.actions.ActionSearchState
 
 class DefAction(term: Term) extends Action {
   private val letAction: LetAction = new LetAction(term)
+  private val updatedTerm = {
+    val toSwitch = term.leaves.filter(_.root.literal.toString.startsWith("?"))
+    val switchTo = toSwitch.map(t => new Tree(new Identifier(t.root.literal.toString.drop(1), t.root.kind, t.root.ns)))
+    term.replaceDescendants(toSwitch zip switchTo toList)
+  }
 
   override def apply(state: ActionSearchState): ActionSearchState = {
     val newProgs =
-      if (term.root == Language.directedLetId) state.programs.addTerm(term.subtrees(1))
-      else state.programs.addTerm(term.subtrees(0)).addTerm(term.subtrees(1))
-    new ActionSearchState(newProgs, letAction(state).rewriteRules)
+      if (term.root == Language.directedLetId) state.programs.addTerm(updatedTerm.subtrees(1))
+      else state.programs.addTerm(updatedTerm.subtrees(0)).addTerm(updatedTerm.subtrees(1))
+    ActionSearchState(newProgs, letAction(state).rewriteRules)
   }
 }
