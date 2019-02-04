357af32175edcf7b60f3a4107ed0c8fe4bd78d1d
Eytan Singher <eytan.singher@gmail.com>
2019-02-04 08:55:52 +0200

    Actions: Using both associative and simple rules.

--- src/main/scala/synthesis/ui/Interpreter.scala
+++ src/main/scala/synthesis/ui/Interpreter.scala
@@ -4,7 +4,7 @@ import java.io.{BufferedReader, PrintStream}
 
 import syntax.AstSugar.Term
 import language.Parser
-import synthesis.{Programs, SimpleRewriteRulesDB}
+import synthesis.{AssociativeRewriteRulesDB, Programs, SimpleRewriteRulesDB}
 import synthesis.actions.ActionSearchState
 import synthesis.actions.operators.UserAction
 
@@ -19,7 +19,7 @@ class Interpreter(terms: Iterator[Term], userOutput: PrintStream) {
 
   def start: Unit = {
     var oldState: ActionSearchState = null
-    var newState = ActionSearchState(Programs.empty, SimpleRewriteRulesDB().rewriteRules)
+    var newState = ActionSearchState(Programs.empty, AssociativeRewriteRulesDB.rewriteRules ++  SimpleRewriteRulesDB().rewriteRules)
     do {
       oldState = newState
       for(action <- actions) {
