444a385bd23e63a0bac4a128de9c76ada8f93171
Tomer Ghelber <tomergelber@gmail.com>
2019-02-17 17:10:07 +0200

    Structures: removed function

--- src/main/scala/structures/HyperGraphManyWithOrderToOne.scala
+++ src/main/scala/structures/HyperGraphManyWithOrderToOne.scala
@@ -5,14 +5,4 @@ package structures
   * @since 11/15/18
   */
 trait HyperGraphManyWithOrderToOne[Node, EdgeType]
-  extends HyperGraphManyWithOrderToOneLike[Node, EdgeType, HyperGraphManyWithOrderToOne[Node, EdgeType]] {
-
-  /** Finds subgraphs by a pattern graph.
-    *
-    * @param hyperPattern The pattern graph to match with
-    * @tparam Id A reference type to show a wanted connection in the pattern.
-    * @return The matched references.
-    */
-  def findSubgraph[Id](hyperPattern: HyperGraphManyWithOrderToOne[Item[Node, Id], Item[EdgeType, Id]]): Set[(Map[Id, Node], Map[Id, EdgeType])] =
-    findSubgraph[Id, HyperGraphManyWithOrderToOne[Item[Node, Id], Item[EdgeType, Id]]](hyperPattern)
-}
+  extends HyperGraphManyWithOrderToOneLike[Node, EdgeType, HyperGraphManyWithOrderToOne[Node, EdgeType]]
