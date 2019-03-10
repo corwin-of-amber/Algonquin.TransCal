9424e2af51291eab0117d9d7c64113b246b962a2
Tomer Ghelber <tomergelber@gmail.com>
2019-03-10 15:54:03 +0200

    Added a complex type

--- src/test/scala/transcallang/ParserTest.scala
+++ src/test/scala/transcallang/ParserTest.scala
@@ -254,6 +254,18 @@ abstract class ParserTest(protected val p: Parser[Term]) extends FunSuite with M
     parsed.subtrees(1).subtrees(1).subtrees(1).root.literal shouldEqual "int"
   }
 
+  test("Parse identifier polymorphic type of polymorphic") {
+    val parsed = (new TranscalParser).apply("_: list<list<int>> -> x")
+    parsed.subtrees(0).root shouldEqual Language.typeBuilderId
+    parsed.subtrees(0).subtrees(0).root.literal shouldEqual "_"
+    parsed.subtrees(0).subtrees(1).root shouldEqual Language.innerTypeId
+    parsed.subtrees(0).subtrees(1).subtrees(0).root.literal shouldEqual "list"
+    parsed.subtrees(0).subtrees(1).subtrees(1).root shouldEqual Language.typeBuilderId
+    parsed.subtrees(0).subtrees(1).subtrees(1).subtrees(0).root.literal shouldEqual "list"
+    parsed.subtrees(0).subtrees(1).subtrees(1).subtrees(1).root.literal shouldEqual "int"
+    parsed.subtrees(1).root.literal shouldEqual "x"
+  }
+
   test("Parse identifier map type") {
     val parsed = (new TranscalParser).apply("_: int :> int -> x")
     parsed.subtrees(0).root shouldEqual Language.typeBuilderId
