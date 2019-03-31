0c5ef2e14aa940f845fd4f6cd65a8e6b90861f8f
eytan.s <eytan.singher@gmail.com>
2019-03-31 16:17:14 +0300

    SPBE: Added another test for ObsEquiv and needed to deepen the search.

--- src/main/scala/synthesis/actions/operators/ObservationalEquivalence.scala
+++ src/main/scala/synthesis/actions/operators/ObservationalEquivalence.scala
@@ -19,7 +19,7 @@ object ObservationalEquivalence {
     val anchors = anchorToTerm.keys.toSet
     val searchGraph = allInOne.addEdges(anchors)
     var rewriteState = new RewriteSearchState(searchGraph)
-    for (i <- 1 to 5; op <- rewriteRules) rewriteState = op(rewriteState)
+    for (i <- 1 to 10; op <- rewriteRules) rewriteState = op(rewriteState)
     val termToTarget: Map[Term, HyperTermId] = anchorToTerm.map(t => (t._2, rewriteState.graph.findEdges(t._1.edgeType).head.target))
     termToTarget.groupBy(t => t._2).map(_._2.keys.toSet).toSet
   }
--- src/test/scala/synthesis/actions/operators/ObservationalEquivalenceTest.scala
+++ src/test/scala/synthesis/actions/operators/ObservationalEquivalenceTest.scala
@@ -14,4 +14,16 @@ class ObservationalEquivalenceTest extends FunSuite with Matchers {
     equivs.head.size shouldEqual (2)
   }
 
+  test("testGetEquives on filtermap And mapfilter applications") {
+    val mapRules = new LetAction(parser("map ?f ?l = l match ((⟨⟩ => ⟨⟩) / ((?x :: ?xs) => f x :: map f xs))")).rules
+    val filterRules = new LetAction(parser("filter ?p ?l = l match ((⟨⟩ => ⟨⟩) / ((?x :: ?xs) => ( (p x) match (true => x :: (filter p xs)) / false => filter p xs) ))")).rules
+    val predRules = new LetAction(parser("pred ?t = t match ((x => true) / (y => false))")).rules
+    val pred2Rules = new LetAction(parser("pred2 ?t = t match ((id x => true) / (id y => false))")).rules
+    val mapperRules = new LetAction(parser("mapper ?t = id t")).rules
+    val tempTerm = parser("filter(pred2, map(mapper, x :: y :: nil)) = map(mapper, filter(pred, x :: y :: nil))")
+    val equivs = ObservationalEquivalence.getEquives(predRules ++ pred2Rules ++ mapperRules ++ mapRules ++ filterRules ++ SimpleRewriteRulesDB.rewriteRules ++ AssociativeRewriteRulesDB.rewriteRules, Seq(tempTerm.subtrees(0), tempTerm.subtrees(1)))
+    equivs.size shouldEqual (1)
+    equivs.head.size shouldEqual (2)
+  }
+
 }
