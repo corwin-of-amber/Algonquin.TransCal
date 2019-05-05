543168ef0dc03cdfcaed83cc0f302a4d0190b758
Tomer Ghelber <tomergelber@gmail.com>
2019-05-05 15:59:38 +0300

    Using FunSuite and added test

--- src/test/scala/synthesis/rewrites/FunctionReturnTypeRewritePropSpec.scala
+++ src/test/scala/synthesis/rewrites/FunctionReturnTypeRewritePropSpec.scala
@@ -1,6 +1,6 @@
 package synthesis.rewrites
 
-import org.scalatest.PropSpec
+import org.scalatest.FunSuite
 import org.scalatest.prop.Checkers
 import synthesis.Programs
 import transcallang.{Language, TranscalParser}
@@ -9,8 +9,8 @@ import transcallang.{Language, TranscalParser}
   * @author tomer
   * @since 2/18/19
   */
-class FunctionReturnTypeRewritePropSpec extends PropSpec with Checkers {
-  property("Finds in the graph") {
+class FunctionReturnTypeRewritePropSpec extends FunSuite with Checkers {
+  test("Finds in the graph") {
     val term = new TranscalParser().apply("cl = (concat: list :> list :> list) al bl")
     val programs = Programs(term)
     val graph = programs.hyperGraph
@@ -18,7 +18,19 @@ class FunctionReturnTypeRewritePropSpec extends PropSpec with Checkers {
     val newGraph = FunctionReturnTypeRewrite(new RewriteSearchState(graph)).graph
     val actualNewEdges = newGraph.edges -- graph.edges
 
-    check(actualNewEdges.size == 1 & actualNewEdges.head.edgeType.identifier == Language.typeId
-      & actualNewEdges.head.metadata.iterator.contains(FunctionReturnTypeRewrite.ApplyTypeMetadata))
+    actualNewEdges.size == 1 & actualNewEdges.head.edgeType.identifier == Language.typeId &
+      actualNewEdges.head.metadata.iterator.contains(FunctionReturnTypeRewrite.ApplyTypeMetadata)
+  }
+
+  test("Finds in the graph number 2") {
+    val term = new TranscalParser().apply("bl = (flatten: list list :> list) al")
+    val programs = Programs(term)
+    val graph = programs.hyperGraph
+
+    val newGraph = FunctionReturnTypeRewrite(new RewriteSearchState(graph)).graph
+    val actualNewEdges = newGraph.edges -- graph.edges
+
+    actualNewEdges.size == 1 & actualNewEdges.head.edgeType.identifier == Language.typeId &
+      actualNewEdges.head.metadata.iterator.contains(FunctionReturnTypeRewrite.ApplyTypeMetadata)
   }
 }
