46261a76c42856a73ff1ca9c0dc17093e546ad51
Tomer Ghelber <tomergelber@gmail.com>
2019-05-05 15:45:19 +0300

    Fixed to work

--- src/main/scala/synthesis/rewrites/FunctionReturnTypeRewrite.scala
+++ src/main/scala/synthesis/rewrites/FunctionReturnTypeRewrite.scala
@@ -1,14 +1,11 @@
 package synthesis.rewrites
 
-import structures.HyperGraphManyWithOrderToOneLike.HyperEdgePattern
 import structures._
-import structures.immutable.{HyperGraphManyWithOrderToOne, VersionedHyperGraph, VocabularyHyperGraph}
+import structures.immutable.HyperGraphManyWithOrderToOne
+import synthesis.HyperTermIdentifier
 import synthesis.rewrites.rewrites._
-import synthesis.search.{Operator, VersionedOperator}
-import synthesis.{HyperTermId, HyperTermIdentifier}
+import synthesis.search.VersionedOperator
 import transcallang.Language
-
-import scala.annotation.tailrec
 
 
 /** This rewrite rule finds functions return types by looking on their types.
@@ -24,36 +21,24 @@ object FunctionReturnTypeRewrite extends VersionedOperator[RewriteSearchState] {
   private val functionIdHole = Hole(1)
   private val functionTypeIdHole = Hole(2)
   private val functionIdentifierHole = Hole(3)
+  private val functionReturnTypeHole = Hole(4)
+  private val functionApplicationHole = Hole(5)
 
   // Used edges
   private val trueEdge = patternEdgeCreator(trueIdHole, Language.trueId, Seq())
   private val functionTypeEdge = patternEdgeCreator(trueIdHole, Language.typeId, Seq(functionIdHole, functionTypeIdHole))
   private val functionIdEdge = patternEdgeCreator(functionIdHole, functionIdentifierHole, Seq())
 
-  @tailrec
-  private def createMapTypeEdge(baseId: Item[HyperTermId, Int], holeCreator: () => Int, counter: Int, createdEdges: Set[HyperEdgePattern[HyperTermId, HyperTermIdentifier, Int]]): (Set[HyperEdgePattern[HyperTermId, HyperTermIdentifier, Int]], Hole[HyperTermId, Int]) = {
-    val newBaseId = Hole(holeCreator())
-    if (counter == 0) {
-      (createdEdges + patternEdgeCreator(baseId, Language.mapTypeId, Seq(Ignored(), newBaseId)), newBaseId)
-    } else {
-      val newCreatedEdges = createdEdges + patternEdgeCreator(baseId, Language.mapTypeId, Seq(Ignored(), newBaseId))
-      createMapTypeEdge(newBaseId, holeCreator, counter - 1, newCreatedEdges)
-    }
-  }
-  private val functionReturnTypeEdge = patternEdgeCreator(functionTypeIdHole, Language.mapTypeId, Seq(Ignored(), Ignored()))
+  private val functionReturnTypeEdge = patternEdgeCreator(functionTypeIdHole, Language.mapTypeId, Seq(Ignored(), Ignored(), functionReturnTypeHole))
+  private val functionApplicationEdge = patternEdgeCreator(functionApplicationHole, functionIdentifierHole, Seq(Ignored(), Ignored()))
 
-  private val funcTypeGraph = HyperGraphManyWithOrderToOne(trueEdge, functionTypeEdge, functionIdEdge, functionReturnTypeEdge)
+  private val funcTypeGraph = HyperGraphManyWithOrderToOne(trueEdge, functionTypeEdge, functionIdEdge, functionReturnTypeEdge, functionApplicationEdge)
 
   override def apply(state: RewriteSearchState, version: Long): (RewriteSearchState, Long) = {
     val newFuncEdges = for (
-      (idMap, identMap) <- state.graph.findSubgraphVersioned[Int](funcTypeGraph, version);
-      functionEdges = state.graph.findEdges(identMap(functionIdentifierHole.id)).filter(_.sources.nonEmpty);
-      numberOfArguments = functionEdges.map(_.sources.size).max;
-      functionEdge <- functionEdges;
-      (edges, last) = createMapTypeEdge(Explicit(idMap(functionTypeIdHole.id)), Stream.from(1 + functionTypeIdHole.id).toIterator.next, numberOfArguments - 2, Set.empty);
-      (typeIdMap, _) <- state.graph.findSubgraphVersioned[Int](VocabularyHyperGraph(edges.toSeq:_ *), version)
+      (idMap, _) <- state.graph.findSubgraphVersioned[Int](funcTypeGraph, version)
     ) yield {
-      HyperEdge(idMap(trueIdHole.id), HyperTermIdentifier(Language.typeId), Seq(functionEdge.target, typeIdMap(last.id)), ApplyTypeMetadata)
+        HyperEdge(idMap(trueIdHole.id), HyperTermIdentifier(Language.typeId), Seq(idMap(functionApplicationHole.id), idMap(functionReturnTypeHole.id)), ApplyTypeMetadata)
     }
 
     val newGraph = state.graph.addEdges(newFuncEdges)
