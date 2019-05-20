76e0eb83c9ce2991f5b04b746ee799054e375526
Tomer Ghelber <tomergelber@gmail.com>
2019-05-20 14:17:22 +0300

    Case class were wrong

--- src/main/scala/synthesis/complexity/Complexity.scala
+++ src/main/scala/synthesis/complexity/Complexity.scala
@@ -15,19 +15,25 @@ object Complexity {
 }
 object ConstantComplexity extends Complexity
 case class ContainerComplexity(contained: Any) extends Complexity
-case class LogComplexity private (inner: Complexity) extends Complexity
+class LogComplexity private (val inner: Complexity) extends Complexity
 object LogComplexity {
   def apply(inner: Complexity): Complexity = inner match {
     case PolynomialComplexity(_, exponent) => exponent
     case _ => new LogComplexity(inner)
   }
+
+  def unapply(arg: LogComplexity): Option[Complexity] = Some(arg.inner)
 }
 case class PolynomialComplexity private (base: Complexity, exponent: Complexity) extends Complexity
-case class AddComplexity private (complexities: Seq[Complexity]) extends Complexity
+class AddComplexity private (val complexities: Seq[Complexity]) extends Complexity
 object AddComplexity {
   def apply(complexities: Seq[Complexity]): AddComplexity = new AddComplexity(complexities.flatMap({ case AddComplexity(inner) => inner ; case x => Some(x)}))
+
+  def unapply(arg: AddComplexity): Option[Seq[Complexity]] = Some(arg.complexities)
 }
-case class MultipleComplexity private (complexities: Seq[Complexity]) extends Complexity
+class MultipleComplexity private (val complexities: Seq[Complexity]) extends Complexity
 object MultipleComplexity {
   def apply(complexities: Seq[Complexity]): MultipleComplexity = new MultipleComplexity(complexities.flatMap({ case MultipleComplexity(inner) => inner ; case x => Some(x)}))
+
+  def unapply(arg: MultipleComplexity): Option[Seq[Complexity]] = Some(arg.complexities)
 }
