940440af864d1041ca8996667feda6c3ef316aec
Tomer Ghelber <tomergelber@gmail.com>
2019-06-10 15:48:25 +0300

    Stopping round robin if nothing changed

--- src/main/scala/synthesis/search/NaiveSearch.scala
+++ src/main/scala/synthesis/search/NaiveSearch.scala
@@ -21,20 +21,28 @@ class NaiveSearch[S <: State, SS <: SearchSpace[S]] extends SearchDepth[S, SS, S
     val operatorVer: mutable.Map[Operator[S], Long] = mutable.Map.empty
     var i = 0
 
-    while (i < maxDepth && !searchSpace.isGoal(state)) {
-      val op = searchSpace.operators(state).drop(i % searchSpace.operators(state).size).head
-      state = op match {
-        case versionedOp: VersionedOperator[S] =>
-          val version = operatorVer.getOrElse(versionedOp, 0l)
-          val (s, newVer) = versionedOp.apply(state, 0)
-          operatorVer.put(versionedOp, newVer)
-          s
-        case _ =>
-          logger.warn(s"Using a non versioned operator $op")
-          op(state)
+    var oldState: Option[S] = Option.empty
+    while (!oldState.contains(state) && i < maxDepth && !searchSpace.isGoal(state)) {
+      oldState = Some(state)
+      import scala.util.control.Breaks._
+      breakable {
+        for (op <- searchSpace.operators(state)) {
+          state = op match {
+            case versionedOp: VersionedOperator[S] =>
+              val version = operatorVer.getOrElse(versionedOp, 0l)
+              val (s, newVer) = versionedOp.apply(state, 0)
+              operatorVer.put(versionedOp, newVer)
+              s
+            case _ =>
+              logger.warn(s"Using a non versioned operator $op")
+              op(state)
+          }
+          i += 1
+          if (searchSpace.isGoal(state)) {
+            break
+          }
+        }
       }
-      i += 1
-      if (i % searchSpace.operators(state).size == 0 && state.isInstanceOf[RewriteSearchState])
         logger.debug(s"Done a round robin. Graph size is: ${state.asInstanceOf[RewriteSearchState].graph.size}")
     }
 
