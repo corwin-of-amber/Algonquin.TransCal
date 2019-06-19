856e81c6bb520f407abd3faa9125420fc8626efe
Tomer Ghelber <tomergelber@gmail.com>
2019-06-19 16:40:14 +0300

    Sorting staff with time complex

--- src/main/scala/synthesis/ui/Main.scala
+++ src/main/scala/synthesis/ui/Main.scala
@@ -3,9 +3,14 @@ package synthesis.ui
 import java.io.{PrintStream, File => JFile}
 
 import org.rogach.scallop.ScallopOption
-import transcallang.{AnnotatedTree, TranscalParser}
+import synthesis.Programs
+import synthesis.complexity.Complexity._
+import synthesis.complexity.{Complexity, ComplexityPartialOrdering, ConstantComplexity, ContainerComplexity}
+import synthesis.rewrites.RewriteRule
+import transcallang.{AnnotatedTree, Identifier, TranscalParser}
 
 import scala.io.Source
+import scala.util.Try
 
 /**
   * @author tomer
@@ -51,5 +56,39 @@ object Main extends App {
   val userOutput: PrintStream = Console.out // conf.file.map(name => new PrintStream(name + ".out")).getOrElse(Console.out)
   val interpreter = new Interpreter(userInput, userOutput)
 
-  interpreter.start()
+  val lastState = interpreter.start()
+
+  implicit object AnnotatedTreeByDepth extends Ordering[AnnotatedTree] {
+    override def compare(x: AnnotatedTree, y: AnnotatedTree): Int = x.toString.compareTo(y.toString)
+  }
+  val TIMECOMPLEX = "timecomplex"
+  val TIMECOMPLEX_IDENTIFIER = Identifier(TIMECOMPLEX)
+  val TIMECOMPLEX_PATTERN: RewriteRule.HyperPattern = Programs.destructPattern(new TranscalParser().parseExpression(f"$TIMECOMPLEX(_, _)"))
+
+  def calculateComplex(tree: AnnotatedTree): Complexity = {
+    tree.root.literal match {
+      case "+" =>
+        val first = calculateComplex(tree.subtrees.head)
+        val second = calculateComplex(tree.subtrees(1))
+        val added = first + second
+        added
+      case literal: String if Try(literal.toInt).isSuccess => ConstantComplexity(literal.toInt)
+      case _ => ContainerComplexity(Programs.termToString(tree))
+    }
+  }
+
+  object FullComplexityPartialOrdering extends Ordering[Complexity] {
+    override def compare(x: Complexity, y: Complexity): Int = {
+      val res = ComplexityPartialOrdering.tryCompare(x, y)
+        res.getOrElse(y.toString compare x.toString)
+    }
+  }
+
+  val fullProgram = lastState.programs
+  fullProgram.hyperGraph.edges.filter(_.edgeType.identifier == TIMECOMPLEX_IDENTIFIER)
+    .toStream.flatMap(e => fullProgram.reconstructWithPattern(e.target, TIMECOMPLEX_PATTERN))
+    .map(tree=> (Programs.termToString(tree.subtrees.head), calculateComplex(tree.subtrees(1))))
+    .groupBy(_._1).map(a => a.copy(_2 = a._2.map(_._2).min(FullComplexityPartialOrdering)))
+    .filter(_._2 != ConstantComplexity(0))
+    .foreach(println)
 }
