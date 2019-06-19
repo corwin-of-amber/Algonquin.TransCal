f686dc24329838d285af9842db8de517d5ca9872
Tomer Ghelber <tomergelber@gmail.com>
2019-06-19 15:42:53 +0300

    Added equal and toString

--- src/main/scala/synthesis/complexity/Complexity.scala
+++ src/main/scala/synthesis/complexity/Complexity.scala
@@ -13,9 +13,20 @@ object Complexity {
     def *(that: Complexity): MultipleComplexity = MultipleComplexity(Seq(complexity, that))
   }
 }
-object ConstantComplexity extends Complexity
-case class ContainerComplexity(contained: Any) extends Complexity
-class LogComplexity private (val inner: Complexity) extends Complexity
+case class ConstantComplexity(constant: Int) extends Complexity {
+  override def toString: String = constant.toString
+}
+case class ContainerComplexity(contained: Any) extends Complexity {
+  override def toString: String = contained.toString
+}
+class LogComplexity private (val inner: Complexity) extends Complexity {
+  override def toString: String = f"log($inner)"
+
+  override def equals(obj: Any): Boolean = obj match {
+    case LogComplexity(other) => inner.equals(other)
+    case _ => super.equals(obj)
+  }
+}
 object LogComplexity {
   def apply(inner: Complexity): Complexity = inner match {
     case PolynomialComplexity(_, exponent) => exponent
@@ -25,13 +36,27 @@ object LogComplexity {
   def unapply(arg: LogComplexity): Option[Complexity] = Some(arg.inner)
 }
 case class PolynomialComplexity private (base: Complexity, exponent: Complexity) extends Complexity
-class AddComplexity private (val complexities: Seq[Complexity]) extends Complexity
+class AddComplexity private (val complexities: Seq[Complexity]) extends Complexity {
+  override def toString: String = complexities.mkString(" + ")
+
+  override def equals(obj: Any): Boolean = obj match {
+    case AddComplexity(others) => complexities.equals(others)
+    case _ => super.equals(obj)
+  }
+}
 object AddComplexity {
   def apply(complexities: Seq[Complexity]): AddComplexity = new AddComplexity(complexities.flatMap({ case AddComplexity(inner) => inner ; case x => Some(x)}))
 
   def unapply(arg: AddComplexity): Option[Seq[Complexity]] = Some(arg.complexities)
 }
-class MultipleComplexity private (val complexities: Seq[Complexity]) extends Complexity
+class MultipleComplexity private (val complexities: Seq[Complexity]) extends Complexity {
+  override def toString: String = complexities.mkString(" * ")
+
+  override def equals(obj: Any): Boolean = obj match {
+    case MultipleComplexity(others) => complexities.equals(others)
+    case _ => super.equals(obj)
+  }
+}
 object MultipleComplexity {
   def apply(complexities: Seq[Complexity]): MultipleComplexity = new MultipleComplexity(complexities.flatMap({ case MultipleComplexity(inner) => inner ; case x => Some(x)}))
 
--- src/main/scala/synthesis/complexity/ComplexityPartialOrdering.scala
+++ src/main/scala/synthesis/complexity/ComplexityPartialOrdering.scala
@@ -8,9 +8,9 @@ object ComplexityPartialOrdering extends PartialOrdering[Complexity] {
   override def tryCompare(x: Complexity, y: Complexity): Option[Int] = {
     (x, y) match {
       case (ContainerComplexity(containedX), ContainerComplexity(containedY)) => if (containedX == containedY) Some(0) else None
-      case (ConstantComplexity, ConstantComplexity) => Some(0)
-      case (ConstantComplexity, _) => Some(1)
-      case (_, ConstantComplexity) => Some(-1)
+      case (ConstantComplexity(_), ConstantComplexity(_)) => Some(0)
+      case (ConstantComplexity(_), _) => Some(1)
+      case (_, ConstantComplexity(_)) => Some(-1)
       case (LogComplexity(innerX), LogComplexity(innerY)) => tryCompare(innerX, innerY)
       case (PolynomialComplexity(baseX, exponentX), PolynomialComplexity(baseY, exponentY)) =>
         tryCompare(baseX, baseY) match {
