953e5d705c71c2c043eb507747eede6a0976b3d9
Tomer Ghelber <tomergelber@gmail.com>
2019-06-27 17:53:22 +0300

    Simplify Vocabulary

--- src/main/scala/structures/VocabularyLike.scala
+++ src/main/scala/structures/VocabularyLike.scala
@@ -2,14 +2,15 @@ package structures
 
 import structures.VocabularyLike.Word
 
-import scala.collection.IterableLike
+import scala.collection.generic.Subtractable
+import scala.collection.{GenTraversableOnce, IterableLike}
 
 /**
   * @author tomer
   * @since 11/15/18
   */
 trait VocabularyLike[Letter, +This <: VocabularyLike[Letter, This]]
-  extends IterableLike[Word[Letter], This] {
+  extends IterableLike[Word[Letter], This] with Subtractable[Word[Letter], This] {
   // TODO: extend Set when we understand how
 
   import VocabularyLike.WordRegex
@@ -41,31 +42,19 @@ trait VocabularyLike[Letter, +This <: VocabularyLike[Letter, This]]
     */
   def replace(keep: Letter, change: Letter): This
 
-  /** Removes an word from the vocabulary.
-    *
-    * @param word The word to remove.
-    * @return The new vocabulary without the edge.
-    */
-  def remove(word: Word[Letter]): This
-  def -(word:Word[Letter]): This = remove(word)
-
   /** Adds word.
     *
     * @param word The word to add.
     * @return The vocabulary with the word.
     */
-  def add(word: Word[Letter]): This
-  def +(word:Word[Letter]): This = add(word)
+  def +(word:Word[Letter]): This
 
   /** Adds words.
     *
     * @param words The words to add.
     * @return The vocabulary with the words.
     */
-  def addAll(words: Set[Word[Letter]]): This
-  def :+(words:Set[Word[Letter]]): This = addAll(words)
-
-  def ++[Other <: VocabularyLike[Letter, Other]](other: Other): This = this :+ other.words
+  def ++[Other <: GenTraversableOnce[Seq[Letter]]](words: Other): This = (repr /: words.seq)(_ + _)
 
   /* --- IterableLike Impl. --- */
 
