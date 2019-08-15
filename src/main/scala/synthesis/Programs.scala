86dbb4fdb63cf123288d7772a5dd48144a22a92e
eytan.s <eytan.singher@gmail.com>
2019-08-15 14:17:15 +0300

    Programs: Destruct managing merging of nodes in typing

--- src/main/scala/synthesis/Programs.scala
+++ src/main/scala/synthesis/Programs.scala
@@ -278,7 +278,7 @@ object Programs extends LazyLogging {
       case Some(annotatedTree) =>
         val (targetType, hyperEdgesType) = innerDestruct(annotatedTree, nodeCreator, identToEdge)
         val trueNode = nodeCreator.next()
-        val functionNode = nodeCreator.next()
+        val functionNode = if (newHyperEdges.exists(e => e.sources.isEmpty && e.edgeType == identToEdge(function.copy(annotation = None)))) target else nodeCreator.next()
         hyperEdgesType ++ Set(
           HyperEdge(functionNode, identToEdge(function.copy(annotation = None)), Seq.empty, EmptyMetadata),
           HyperEdge(trueNode, identToEdge(Language.typeId), Seq(functionNode, targetType), EmptyMetadata),
