ad1e59e984b6e8e6ef34703df1393417f31e576b
Tomer Ghelber <tomergelber@gmail.com>
2019-08-26 18:47:29 +0300

    Added tuple time complex

---
+++ src/main/scala/synthesis/rewrites/TupleTimeComplexRewrite.scala
@@ -0,0 +1,52 @@
+package synthesis.rewrites
+
+import structures.{EmptyMetadata, HyperEdge, Ignored}
+import synthesis.rewrites.Template.{ExplicitTerm, ReferenceTerm, RepetitionTerm, TemplateTerm}
+import synthesis.search.VersionedOperator
+import synthesis.{HyperTermId, HyperTermIdentifier, Programs}
+import transcallang.Language
+
+/** Creating tuple expression's time complex edge.
+  *
+  */
+object TupleTimeComplexRewrite extends VersionedOperator[RewriteSearchState]{
+
+  /* --- Private --- */
+
+  private val tupleTimeComplexEdge = HyperEdge(ReferenceTerm(0), ExplicitTerm(HyperTermIdentifier(Language.tupleId)), Seq(RepetitionTerm.rep1(Int.MaxValue, Stream.from(1).map(ReferenceTerm(_))).get), EmptyMetadata)
+
+  private def createTimeComplexRegex(id: HyperTermId): HyperEdge[TemplateTerm[HyperTermId], TemplateTerm[HyperTermIdentifier]] = {
+    HyperEdge(ReferenceTerm(0), ExplicitTerm(HyperTermIdentifier(Language.timeComplexId)), Seq(ExplicitTerm(id), ReferenceTerm(1)), EmptyMetadata)
+  }
+
+  /* --- VersionedOperator Impl. --- */
+
+  override def apply(state: RewriteSearchState, lastVersion: Long): (RewriteSearchState, Long) = {
+    val timeComplexTrueEdge = HyperEdge(Ignored(), ExplicitTerm(HyperTermIdentifier(Language.timeComplexTrueId)), Seq.empty, EmptyMetadata)
+    val timeComplexTrueIdOption = state.graph.findRegexHyperEdges(timeComplexTrueEdge).map(_.target).headOption
+    timeComplexTrueIdOption match {
+      case None => (state, lastVersion)
+      case Some(timeComplexTrueId) =>
+    val nodeCreator = Stream.from(state.graph.nodes.map(_.id).max + 1).iterator.map(HyperTermId)
+
+    val newMatchTimeComplexEdges = state.graph.findRegexHyperEdges(tupleTimeComplexEdge).flatMap(
+      edge => {
+        val tupleExpressionNodeId = edge.target
+        val sourcesTimeComplexes = edge.sources.map{id =>
+          val idRegex = createTimeComplexRegex(id)
+          state.graph.findRegexHyperEdges(idRegex).map(_.sources(1)).iterator
+        }
+        println("sourcesTimeComplexes - " + sourcesTimeComplexes.count(_.hasNext) + " size - " + sourcesTimeComplexes.size)
+        Programs.combineSeq(sourcesTimeComplexes).flatMap(timeComplexSources => {
+          val (timeComplexBaseId, timeComplexEdges) = timeComplexSources.tail.foldLeft((timeComplexSources.head, Set.empty[HyperEdge[HyperTermId, HyperTermIdentifier]])){case ((baseId, edges), newId) =>
+            val tupleTimeComplexId = nodeCreator.next()
+            (tupleTimeComplexId, edges + HyperEdge[HyperTermId, HyperTermIdentifier](tupleTimeComplexId, HyperTermIdentifier(Language.plusId), Seq(baseId, newId), EmptyMetadata))
+          }
+          timeComplexEdges + HyperEdge(timeComplexTrueId, HyperTermIdentifier(Language.timeComplexId), Seq(tupleExpressionNodeId, timeComplexBaseId), EmptyMetadata)
+        })
+    })
+    val newGraph = state.graph ++ newMatchTimeComplexEdges
+    (new RewriteSearchState(newGraph), newGraph.version)
+    }
+  }
+}
