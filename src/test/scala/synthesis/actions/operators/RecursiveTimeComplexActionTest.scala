36716df336f1b1bfc3ff3877a85e09e10b0db1ef
Tomer Ghelber <tomergelber@gmail.com>
2019-08-26 19:02:56 +0300

    fixed real complexes

--- src/test/scala/synthesis/actions/operators/RecursiveTimeComplexActionTest.scala
+++ src/test/scala/synthesis/actions/operators/RecursiveTimeComplexActionTest.scala
@@ -15,17 +15,16 @@ class RecursiveTimeComplexActionTest extends FunSuite with Matchers  {
     val parser = new TranscalParser()
     val terms = List(
       parser.apply("nodup ?w ?l = l match (⟨⟩ => true) / ((?x :: ?xs) => ({x} ∉ w) ∧ (nodup ({x} ∪ w) xs)) [++]"),
-      parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} ?l 0 [++]"),
       parser.apply("a1 -> ?nodup' {x} xs"),
       parser.apply("xs = x' :: xs'"),
       parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} l 0 [++]"),
       parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} x 0 [++]"),
       parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} xs 0 [++]"),
       parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} w 0 [++]"),
-      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} l 0 [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} l (len l) [++]"),
       parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} x 0 [++]"),
-      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} xs 0 [++]"),
-      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} w 0 [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} xs (len xs) [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} w (len w) [++]"),
       parser.apply("a1 -> (_ ∉ {x}) /\\ ((_ ∪ _) ‖ _) /\\ nodup _"),
     )
     val sumHyperGraph = new Interpreter(terms.iterator, System.out).start().programs
