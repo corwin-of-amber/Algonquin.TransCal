a0e456e4e15690d9ca6868120f4fb3ff89b198a3
eytan.s <eytan.singher@gmail.com>
2019-09-23 14:01:11 +0300

    Actions: Added trait for search actions (so we can use easily inside other actions)

--- src/main/scala/synthesis/actions/operators/Action.scala
+++ src/main/scala/synthesis/actions/operators/Action.scala
@@ -2,6 +2,7 @@ package synthesis.actions.operators
 
 import com.typesafe.scalalogging.LazyLogging
 import synthesis.actions.ActionSearchState
+import synthesis.rewrites.RewriteSearchState
 import synthesis.search.Operator
 
 /**
@@ -9,3 +10,6 @@ import synthesis.search.Operator
   * @since 11/18/18
   */
 trait Action extends Operator[ActionSearchState] with LazyLogging
+trait SearchAction extends Action {
+  def fromRewriteState(state: RewriteSearchState, rules: Set[Operator[RewriteSearchState]]): RewriteSearchState
+}
