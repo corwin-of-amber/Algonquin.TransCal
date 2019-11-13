f831496f4b8fbfdb5599b4b1d671b674a2e0de63
Tomer Ghelber <tomergelber@gmail.com>
2019-11-13 16:58:20 +0200

    Moved tests to base trait

--- src/test/scala/structures/immutable/TriePropSpec.scala
+++ src/test/scala/structures/immutable/TriePropSpec.scala
@@ -1,122 +1,22 @@
 package structures.immutable
 
 import org.scalacheck.Arbitrary
-import org.scalatest.{Assertion, Matchers, PropSpec}
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
-import structures.{Explicit, Hole, Ignored, Repetition}
-
-import scala.util.Random
+import org.scalatest.Assertion
+import structures._
 
 
-class TriePropSpec extends PropSpec with Matchers with ScalaCheckPropertyChecks {
-  private implicit val letterCreator: Arbitrary[Int] = Arbitrary(integerLetterGen)
-  private implicit val wordCreator: Arbitrary[Seq[Int]] = Arbitrary(integerWordGen)
-  private implicit val trieCreator: Arbitrary[Trie[Int]] = Arbitrary(integerTrieGen)
-
-  def checkRemoved(trie: Trie[Int], i: Int): Assertion = {
-    val word = trie.words.toList(i)
-    (trie - word).words should not contain(word)
-  }
-
-  property("all constructor") {
-    forAll { words: Set[Seq[Int]] =>
-      new Trie(words).words shouldEqual words
-      Trie(words).words shouldEqual words
-    }
-  }
-
-  property("removes") {
-    forAll(minSize(1)) { trie: Trie[Int] => whenever(trie.nonEmpty) {
-      checkRemoved(trie, Random.nextInt(trie.words.size))
-    }}
-  }
-
-  property("remove non exists") {
-    forAll { (trie: Trie[Int], word: Seq[Int]) =>
-      whenever(!trie.words.contains(word)) {
-        (trie - word) shouldEqual trie
-      }
-    }
-  }
-
-  property("add non exists works") {
-    forAll { (trie: Trie[Int], word: Seq[Int]) =>
-      whenever(!trie.words.contains(word)) {
-        (trie + word).words should contain (word)
-      }
-    }
-  }
-
-  property("add twice works") {
-    forAll { word: Seq[Int] =>
-        val onceTrie = Trie.empty + word
-        val twiceTrie = onceTrie + word
-        onceTrie shouldEqual twiceTrie
-    }
-  }
-
-  property("empty find prefix returns all") {
-    forAll { trie: Trie[Int] =>
-      trie.words shouldEqual trie.findRegexWords(Seq(Repetition.rep0[Int, Nothing](Int.MaxValue, Ignored()).get))
-    }
-  }
-
-  property("added should be findable as sparse") {
-    forAll { trie: Trie[Int] =>
-      trie.words.forall(word => {
-        val repetitionInf = Repetition.rep0[Int, Nothing](Int.MaxValue, Ignored()).get
-        val regex = word.flatMap(i => Seq(repetitionInf, Explicit(i))) :+ repetitionInf
-        trie.findRegexWords(regex).contains(word)
-      }) shouldBe true
-    }
-  }
-
-  property("added should be findable") {
-    forAll { trie: Trie[Int] =>
-      trie.words.forall(word => trie.findRegexWords(word.map(Explicit(_))).contains(word)) shouldBe true
-    }
-  }
-
-  property("edges finds all that were added twice") {
-    forAll { word: Seq[Int] =>
-      (Trie.empty[Int] + word + word).words.count(_ == word) shouldBe 1
-    }
-  }
-
-  property("edges finds all that were added") {
-    forAll { words: Set[Seq[Int]] =>
-      words.intersect(words.foldLeft(Trie.empty[Int])(_ + _).words) should have size words.size
-    }
-  }
-
-  property("Should find correct explicit in big graph") {
-    forAll(minSize(100)) { trie: Trie[Int] =>
-      whenever(trie.size > 100 && trie.words.exists(_.length > 3)) {
-        trie.words.filter(_.length > 3).forall(w =>
-          trie.findRegexWords(Seq(Hole(0), Hole(1), Hole(2), Explicit(w(3)), Repetition.rep0[Int, Int](Int.MaxValue, Ignored()).get))
-            .forall(_(3) == w(3))) shouldBe true
-      }
-    }
-  }
-
-  property("add than remove than add") {
-    forAll { (trie: Trie[Int], word: Seq[Int]) =>
-      whenever(!trie.words.contains(word)) {
-        val trieAdd = trie + word
-        trie.words should have size (trieAdd.words.size -1)
-        val trieRemove = trie - word
-        trieAdd.words should have size (trieRemove.words.size + 1)
-        trieRemove.words should have size ((trie + word).words.size - 1)
-      }
-    }
-  }
+class TriePropSpec extends VocabularyTest[Int, Trie[Int]] {
+  protected implicit val letterCreator: Arbitrary[Int] = Arbitrary(integerLetterGen)
+  protected implicit val wordCreator: Arbitrary[Seq[Int]] = Arbitrary(integerWordGen)
+  protected implicit val trieCreator: Arbitrary[Trie[Int]] = Arbitrary(integerTrieGen)
+  override def builder(es: Set[VocabularyLike.Word[Int]]) = Trie(es)
 
   property("changed letter to exists letter") {
     def validate(trie: Trie[Int], keepLetter: Int, changeLetter: Int): Assertion = {
       val beforeLettersSize = trie.letters.size
       val newTrie = trie.replace(keepLetter, changeLetter)
       val found = newTrie.findRegex(Seq(Repetition.rep0(Int.MaxValue, Ignored()).get, Explicit(changeLetter), Repetition.rep0(Int.MaxValue, Ignored()).get))
-      newTrie.letters should have size (beforeLettersSize - 1)
+      newTrie.letters should have size beforeLettersSize - 1
       trie should not equal newTrie
       found shouldBe empty
     }
@@ -136,11 +36,13 @@ class TriePropSpec extends PropSpec with Matchers with ScalaCheckPropertyChecks 
       newTrie.letters should have size beforeLettersSize
       newTrie.words should have size beforeWordsSize
       trie should not equal newTrie
-      found shouldBe (empty)
+      found shouldBe empty
     }
-    forAll(minSize(1)) { trie: Trie[Int] => whenever(trie.nonEmpty) {
-      validate(trie, trie.letters.max + 1, trie.letters.last)
-    }}
+    forAll(minSize(1)) { trie: Trie[Int] =>
+      whenever(trie.letters.nonEmpty) {
+        validate(trie, trie.letters.max + 1, trie.letters.last)
+      }
+    }
   }
 
   property("changed non exist letter keeps the trie the same") {
--- src/test/scala/structures/mutable/TriePropSpec.scala
+++ src/test/scala/structures/mutable/TriePropSpec.scala
@@ -1,119 +1,15 @@
 package structures.mutable
 
 import org.scalacheck.Arbitrary
-import org.scalatest.{Assertion, Matchers, PropSpec}
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
-import structures.{Explicit, Hole, Ignored, Repetition}
-
-import scala.util.Random
+import org.scalatest.Assertion
+import structures._
 
 
-class TriePropSpec extends PropSpec with Matchers with ScalaCheckPropertyChecks {
-  private implicit val letterCreator: Arbitrary[Int] = Arbitrary(integerLetterGen)
-  private implicit val wordCreator: Arbitrary[Seq[Int]] = Arbitrary(integerWordGen)
-  private implicit val trieCreator: Arbitrary[Trie[Int]] = Arbitrary(integerTrieGen)
-
-  def checkRemoved(trie: Trie[Int], i: Int): Assertion = {
-    val word = trie.words.toList(i)
-    (trie - word).words should not contain (word)
-  }
-
-  property("all constructor") {
-    forAll { words: Set[Seq[Int]] =>
-      new Trie(words).words shouldEqual words
-      Trie(words).words shouldEqual words
-    }
-  }
-
-  property("removes") {
-    forAll { trie: Trie[Int] =>
-      whenever(trie.words.nonEmpty) {
-        checkRemoved(trie, Random.nextInt(trie.words.size))
-      }
-    }
-  }
-
-  property("remove non exists") {
-    forAll { (trie: Trie[Int], word: Seq[Int]) =>
-      whenever(!trie.words.contains(word)) {
-        (trie - word) shouldEqual trie
-      }
-    }
-  }
-
-  property("add non exists works") {
-    forAll { (trie: Trie[Int], word: Seq[Int]) =>
-      whenever(!trie.words.contains(word)) {
-        (trie + word).words should contain (word)
-      }
-    }
-  }
-
-  property("add twice works") {
-    forAll { word: Seq[Int] =>
-        val onceTrie = Trie.empty + word
-        val twiceTrie = onceTrie + word
-        onceTrie shouldEqual twiceTrie
-    }
-  }
-
-  property("empty find prefix returns all") {
-    forAll { trie: Trie[Int] =>
-      trie.words shouldEqual trie.findRegexWords(Seq(Repetition.rep0[Int, Nothing](Int.MaxValue, Ignored()).get))
-    }
-  }
-
-  property("added should be findable as sparse") {
-    forAll { trie: Trie[Int] =>
-      for (word <- trie.words) {
-        val repetitionInf = Repetition.rep0[Int, Nothing](Int.MaxValue, Ignored()).get
-        val regex = word.flatMap(i => Seq(repetitionInf, Explicit(i))) :+ repetitionInf
-        trie.findRegexWords(regex) should contain (word)
-      }
-    }
-  }
-
-  property("added should be findable") {
-    forAll { trie: Trie[Int] =>
-      for(word <- trie.words) {
-        trie.findRegexWords(word.map(Explicit(_))) should contain (word)
-      }
-    }
-  }
-
-  property("edges finds all that were added twice") {
-    forAll { word: Seq[Int] =>
-      (Trie.empty[Int] + word + word).words.count(_ == word) shouldBe 1
-    }
-  }
-
-  property("edges finds all that were added") {
-    forAll { words: Set[Seq[Int]] =>
-      words.intersect(words.foldLeft(Trie.empty[Int])(_ + _).words) should have size words.size
-    }
-  }
-
-  property("Should find correct explicit in big graph") {
-    forAll { trie: Trie[Int] =>
-      whenever(trie.size > 100 && trie.words.exists(_.length > 3)) {
-      trie.words.filter(_.length > 3).forall(w =>
-        trie.findRegexWords(Seq(Hole(0), Hole(1), Hole(2), Explicit(w(3)), Repetition.rep0[Int, Int](Int.MaxValue, Ignored()).get))
-          .forall(_(3) == w(3))) shouldBe true
-      }
-    }
-  }
-
-  property("add than remove than add") {
-    forAll { (trie: Trie[Int], word: Seq[Int]) =>
-      whenever(!trie.words.contains(word)) {
-        val trieAdd = trie + word
-        trieAdd.words should have size trie.words.size + 1
-        val trieRemove = trie - word
-        trieRemove.words should have size trieAdd.words.size-1
-        (trie + word).words should have size trieRemove.words.size + 1
-      }
-    }
-  }
+class TriePropSpec extends VocabularyTest[Int, Trie[Int]] {
+  protected implicit val letterCreator: Arbitrary[Int] = Arbitrary(integerLetterGen)
+  protected implicit val wordCreator: Arbitrary[Seq[Int]] = Arbitrary(integerWordGen)
+  protected implicit val trieCreator: Arbitrary[Trie[Int]] = Arbitrary(integerTrieGen)
+  override def builder(es: Set[VocabularyLike.Word[Int]]) = Trie(es)
 
   property("changed letter to exists letter") {
     def validate(trie: Trie[Int], keepLetter: Int, changeLetter: Int): Assertion = {
@@ -122,7 +18,7 @@ class TriePropSpec extends PropSpec with Matchers with ScalaCheckPropertyChecks 
       val found = newTrie.findRegex(Seq(Repetition.rep0(Int.MaxValue, Ignored()).get, Explicit(changeLetter), Repetition.rep0(Int.MaxValue, Ignored()).get))
       newTrie.letters should have size beforeLettersSize - 1
       trie should not equal newTrie
-      found shouldBe (empty)
+      found shouldBe empty
     }
     forAll { trie: Trie[Int] =>
       whenever(trie.letters.size > 1) {
@@ -142,7 +38,7 @@ class TriePropSpec extends PropSpec with Matchers with ScalaCheckPropertyChecks 
       trie should not equal newTrie
       found shouldBe empty
     }
-    forAll { trie: Trie[Int] =>
+    forAll(minSize(1)){ trie: Trie[Int] =>
       whenever(trie.letters.nonEmpty) {
         validate(trie, trie.letters.max + 1, trie.letters.last)
       }
@@ -178,7 +74,7 @@ class TriePropSpec extends PropSpec with Matchers with ScalaCheckPropertyChecks 
   }
 
   property("Should find same explicit words in all graphs") {
-    forAll { trie: Trie[Int] => whenever(trie.nonEmpty) {
+    forAll(minSize(1)) { trie: Trie[Int] => whenever(trie.nonEmpty) {
       val byLength = trie.words.groupBy(_.length)
       for ((key, words) <- byLength) {
         val regex = 0 until key map Hole[Int, Int]
