a04b003bf931ce7025f28e86cf1f73827cae8d6f
Tomer Ghelber <tomergelber@gmail.com>
2019-11-13 16:25:29 +0200

    Using Matchers instead of Checkers

--- src/test/scala/structures/immutable/VocabularyHyperGraphPropSpec.scala
+++ src/test/scala/structures/immutable/VocabularyHyperGraphPropSpec.scala
@@ -1,15 +1,11 @@
 package structures.immutable
 
 import org.scalacheck.Arbitrary
-import org.scalatest.{Matchers, PropSpec}
-import org.scalatestplus.scalacheck.Checkers
 import structures.HyperGraphLike.HyperEdgePattern
 import structures._
 
-import scala.util.Random
 
-
-class VocabularyHyperGraphPropSpec extends PropSpec with Checkers with Matchers with HyperGraphLikeTest[Int, Int, VocabularyHyperGraph[Int, Int], VocabularyHyperGraph[Item[Int, Int], Item[Int, Int]]]{
+class VocabularyHyperGraphPropSpec extends HyperGraphLikeTest[Int, Int, VocabularyHyperGraph[Int, Int], VocabularyHyperGraph[Item[Int, Int], Item[Int, Int]]]{
   implicit def edgeCreator: Arbitrary[HyperEdge[Int, Int]] = Arbitrary(integerEdgesGen)
   implicit def graphCreator: Arbitrary[VocabularyHyperGraph[Int, Int]] = Arbitrary(integerGraphGen)
   override implicit def nodeCreator: Arbitrary[Int] = Arbitrary(integerLetterGen)
