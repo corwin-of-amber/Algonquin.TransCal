88c4206d332f96deb1b1c1ffe25d140823cfbe79
Tomer Ghelber <tomergelber@gmail.com>
2019-11-20 10:32:07 +0200

    Added more textual tests

--- src/test/scala/synthesis/actions/operators/RecursiveTimeComplexActionTest.scala
+++ src/test/scala/synthesis/actions/operators/RecursiveTimeComplexActionTest.scala
@@ -39,6 +39,28 @@ class RecursiveTimeComplexActionTest extends FunSuite with Matchers  {
     found should have size(1)
   }
 
+  test("nodup' example textual") {
+    val parser = new TranscalParser()
+    val terms = List(
+      parser.apply("nodup' ?w ?l = l match (⟨⟩ => true) / ((?x :: ?xs) => ({x} ∉ w) ∧ (nodup' ({x} ∪ w) xs)) [++]"),
+      parser.apply("l = x :: xs [++]"),
+      parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} l 0 [++]"),
+      parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} x 0 [++]"),
+      parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} xs 0 [++]"),
+      parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} w 0 [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} l (len l) [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} x 0 [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} xs (len xs) [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} w (len w) [++]"),
+      parser.apply(f"${Language.recursiveTimeComplexId.literal} nodup' 2"),
+    )
+    val lastState = new Interpreter(terms.iterator, System.out).start()
+
+    val wantedGraph = new LetAction(parser.apply(f"${Language.timeComplexId.literal} (nodup' ?w ?l) (len(l)) >> ${Language.timeComplexTrueId.literal}")).rules.head.premise
+    val found = lastState.programs.hyperGraph.findSubgraph[Int](wantedGraph)
+    found should have size (1)
+  }
+
   test("sum example") {
     val testedAction = new RecursiveTimeComplexAction(Identifier("sum"), 1)
     val parser = new TranscalParser()
@@ -63,6 +85,26 @@ class RecursiveTimeComplexActionTest extends FunSuite with Matchers  {
 
     val wantedGraph = new LetAction(parser.apply(f"${Language.timeComplexId.literal} (sum ?l) (len(l)) >> ${Language.timeComplexTrueId.literal}")).rules.head.premise
     val found = newPrograms.programs.hyperGraph.findSubgraph[Int](wantedGraph)
+    found should have size(1)
+  }
+
+  test("sum example textual") {
+    val parser = new TranscalParser()
+    val terms = List(
+      parser.apply("sum ?l = l match (⟨⟩ => 0) / ((?x :: ?xs) => 1 + (sum xs)) [++]"),
+      parser.apply("l = x :: xs [++]"),
+      parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} l 0 [++]"),
+      parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} x 0 [++]"),
+      parser.apply(f"${Language.timeComplexTrueId.literal} = ${Language.timeComplexId.literal} xs 0 [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} l (len l) [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} x 0 [++]"),
+      parser.apply(f"${Language.spaceComplexTrueId.literal} = ${Language.spaceComplexId.literal} xs (len xs) [++]"),
+      parser.apply(f"${Language.recursiveTimeComplexId.literal} sum 1"),
+    )
+    val lastState = new Interpreter(terms.iterator, System.out).start()
+
+    val wantedGraph = new LetAction(parser.apply(f"${Language.timeComplexId.literal} (sum ?l) (len(l)) >> ${Language.timeComplexTrueId.literal}")).rules.head.premise
+    val found = lastState.programs.hyperGraph.findSubgraph[Int](wantedGraph)
     found should have size(1)
   }
 }
