a73b03ffaef1d35008699a2a2ccdcc0ce43b0e90
Tomer Ghelber <tomergelber@gmail.com>
2019-11-26 12:10:36 +0200

    Made them StepOperator

--- src/main/scala/synthesis/rewrites/MatchTimeComplexRewrite.scala
+++ src/main/scala/synthesis/rewrites/MatchTimeComplexRewrite.scala
@@ -1,15 +1,15 @@
 package synthesis.rewrites
 
 import structures.{EmptyMetadata, HyperEdge, Ignored}
-import synthesis.{HyperTermId, HyperTermIdentifier, Programs}
 import synthesis.rewrites.Template.{ExplicitTerm, ReferenceTerm, RepetitionTerm, TemplateTerm}
-import synthesis.search.VersionedOperator
+import synthesis.search.{StepOperator, VersionedOperator}
+import synthesis.{HyperTermId, HyperTermIdentifier, Programs}
 import transcallang.{Identifier, Language}
 
 /** Creating match expression's time complex edge.
   *
   */
-object MatchTimeComplexRewrite extends VersionedOperator[RewriteSearchState] {
+object MatchTimeComplexRewrite extends VersionedOperator[RewriteSearchState] with StepOperator[Set[HyperEdge[TemplateTerm[HyperTermId], TemplateTerm[HyperTermIdentifier]]], RewriteSearchState] {
 
   val MAX_TIME_COMPLEX = "max"
 
@@ -23,29 +23,50 @@ object MatchTimeComplexRewrite extends VersionedOperator[RewriteSearchState] {
 
   /* --- VersionedOperator Impl. --- */
 
-  override def apply(state: RewriteSearchState, lastVersion: Long): (RewriteSearchState, Long) = {
+
+  /** Create an operator that finishes the action of the step operator. This should be used as a way to hold off adding
+    * edges to the graph until all calculations of a step are done.
+    *
+    * @param state       current state from which to do the initial calculations and create an operator
+    * @param lastVersion Version to use if this is a versioned step operator
+    * @return an operator to later on be applied on the state. NOTICE - some operators might need state to not change.
+    */
+  override def getStep(state: RewriteSearchState, lastVersion: Long): Set[HyperEdge[TemplateTerm[HyperTermId], TemplateTerm[HyperTermIdentifier]]] = {
+    getNewEdges(state, lastVersion).map(e=>HyperEdge(
+      ExplicitTerm(e.target),
+      ExplicitTerm(e.edgeType),
+      e.sources.map(ExplicitTerm(_)),
+      e.metadata
+    ))
+  }
+
+  private def getNewEdges(state: RewriteSearchState, lastVersion: Long): Set[HyperEdge[HyperTermId, HyperTermIdentifier]] = {
     val timeComplexTrueEdge = HyperEdge(Ignored(), ExplicitTerm(HyperTermIdentifier(Language.timeComplexTrueId)), Seq.empty, EmptyMetadata)
     val timeComplexTrueIdOption = state.graph.findRegexHyperEdges(timeComplexTrueEdge).map(_.target).headOption
     timeComplexTrueIdOption match {
-      case None => (state, lastVersion)
+      case None => Set.empty
       case Some(timeComplexTrueId) =>
-    val nodeCreator = Stream.from(state.graph.nodes.map(_.id).max + 1).iterator.map(HyperTermId)
+        val nodeCreator = Stream.from(state.graph.nodes.map(_.id).max + 1).iterator.map(HyperTermId)
 
-    val newMatchTimeComplexEdges = state.graph.findRegexHyperEdges(matchTimeComplexEdge).flatMap(
-      edge => {
-        val matchExpressionNodeId = edge.target
-        Programs.combineSeq(edge.sources.map(id =>
-          state.graph.findRegexHyperEdges(createTimeComplexRegex(id)).map(_.sources(1)).toStream
-        )).flatMap(timeComplexSources => {
-          val matchTimeComplexId = nodeCreator.next()
-          Set(
-            HyperEdge(matchTimeComplexId, HyperTermIdentifier(Identifier(MAX_TIME_COMPLEX)), timeComplexSources, EmptyMetadata),
-            HyperEdge(timeComplexTrueId, HyperTermIdentifier(Language.timeComplexId), Seq(matchExpressionNodeId, matchTimeComplexId), EmptyMetadata)
-          )
-        })
-    })
-    val newGraph = state.graph ++ newMatchTimeComplexEdges
-    (new RewriteSearchState(newGraph), newGraph.version)
+        val newMatchTimeComplexEdges = state.graph.findRegexHyperEdges(matchTimeComplexEdge).flatMap(
+          edge => {
+            val matchExpressionNodeId = edge.target
+            Programs.combineSeq(edge.sources.map(id =>
+              state.graph.findRegexHyperEdges(createTimeComplexRegex(id)).map(_.sources(1)).toStream
+            )).flatMap(timeComplexSources => {
+              val matchTimeComplexId = nodeCreator.next()
+              Set(
+                HyperEdge(matchTimeComplexId, HyperTermIdentifier(Identifier(MAX_TIME_COMPLEX)), timeComplexSources, EmptyMetadata),
+                HyperEdge(timeComplexTrueId, HyperTermIdentifier(Language.timeComplexId), Seq(matchExpressionNodeId, matchTimeComplexId), EmptyMetadata)
+              )
+            })
+          })
+        newMatchTimeComplexEdges
     }
+  }
+
+  override def apply(state: RewriteSearchState, lastVersion: Long): (RewriteSearchState, Long) = {
+    state.graph ++= getNewEdges(state, lastVersion)
+    (state, state.graph.version)
   }
 }
--- src/main/scala/synthesis/rewrites/TupleTimeComplexRewrite.scala
+++ src/main/scala/synthesis/rewrites/TupleTimeComplexRewrite.scala
@@ -2,14 +2,14 @@ package synthesis.rewrites
 
 import structures.{EmptyMetadata, HyperEdge, Ignored}
 import synthesis.rewrites.Template.{ExplicitTerm, ReferenceTerm, RepetitionTerm, TemplateTerm}
-import synthesis.search.VersionedOperator
+import synthesis.search.{StepOperator, VersionedOperator}
 import synthesis.{HyperTermId, HyperTermIdentifier, Programs, TimeComplexRewriteRulesDB}
 import transcallang.{Identifier, Language}
 
 /** Creating tuple expression's time complex edge.
   *
   */
-object TupleTimeComplexRewrite extends VersionedOperator[RewriteSearchState]{
+object TupleTimeComplexRewrite extends VersionedOperator[RewriteSearchState] with StepOperator[Set[HyperEdge[TemplateTerm[HyperTermId], TemplateTerm[HyperTermIdentifier]]], RewriteSearchState] {
 
   /* --- Private --- */
 
@@ -21,31 +21,51 @@ object TupleTimeComplexRewrite extends VersionedOperator[RewriteSearchState]{
 
   /* --- VersionedOperator Impl. --- */
 
-  override def apply(state: RewriteSearchState, lastVersion: Long): (RewriteSearchState, Long) = {
+  /** Create an operator that finishes the action of the step operator. This should be used as a way to hold off adding
+    * edges to the graph until all calculations of a step are done.
+    *
+    * @param state       current state from which to do the initial calculations and create an operator
+    * @param lastVersion Version to use if this is a versioned step operator
+    * @return an operator to later on be applied on the state. NOTICE - some operators might need state to not change.
+    */
+  override def getStep(state: RewriteSearchState, lastVersion: Long): Set[HyperEdge[TemplateTerm[HyperTermId], TemplateTerm[HyperTermIdentifier]]] = {
+    getNewEdges(state, lastVersion).map(e=>HyperEdge(
+      ExplicitTerm(e.target),
+      ExplicitTerm(e.edgeType),
+      e.sources.map(ExplicitTerm(_)),
+      e.metadata
+    ))
+  }
+
+  private def getNewEdges(state: RewriteSearchState, lastVersion: Long): Set[HyperEdge[HyperTermId, HyperTermIdentifier]] = {
     val timeComplexTrueEdge = HyperEdge(Ignored(), ExplicitTerm(HyperTermIdentifier(Language.timeComplexTrueId)), Seq.empty, EmptyMetadata)
     val timeComplexTrueIdOption = state.graph.findRegexHyperEdges(timeComplexTrueEdge).map(_.target).headOption
     timeComplexTrueIdOption match {
-      case None => (state, lastVersion)
+      case None => Set.empty
       case Some(timeComplexTrueId) =>
-    val nodeCreator = Stream.from(state.graph.nodes.map(_.id).max + 1).iterator.map(HyperTermId)
+        val nodeCreator = Stream.from(state.graph.nodes.map(_.id).max + 1).iterator.map(HyperTermId)
 
-    val newMatchTimeComplexEdges = state.graph.findRegexHyperEdges(tupleTimeComplexEdge).flatMap(
-      edge => {
-        val tupleExpressionNodeId = edge.target
-        val sourcesTimeComplexes = edge.sources.map{id =>
-          val idRegex = createTimeComplexRegex(id)
-          state.graph.findRegexHyperEdges(idRegex).map(_.sources(1)).iterator
-        }
-        Programs.combineSeq(sourcesTimeComplexes.map(_.toStream)).flatMap(timeComplexSources => {
-          val (timeComplexBaseId, timeComplexEdges) = timeComplexSources.tail.foldLeft((timeComplexSources.head, Set.empty[HyperEdge[HyperTermId, HyperTermIdentifier]])){case ((baseId, edges), newId) =>
-            val tupleTimeComplexId = nodeCreator.next()
-            (tupleTimeComplexId, edges + HyperEdge[HyperTermId, HyperTermIdentifier](tupleTimeComplexId, HyperTermIdentifier(Identifier(TimeComplexRewriteRulesDB.ADD_TIME_COMPLEX)), Seq(baseId, newId), EmptyMetadata))
-          }
-          timeComplexEdges + HyperEdge(timeComplexTrueId, HyperTermIdentifier(Language.timeComplexId), Seq(tupleExpressionNodeId, timeComplexBaseId), EmptyMetadata)
-        })
-    })
-    val newGraph = state.graph ++ newMatchTimeComplexEdges
-    (new RewriteSearchState(newGraph), newGraph.version)
+        val newMatchTimeComplexEdges = state.graph.findRegexHyperEdges(tupleTimeComplexEdge).flatMap(
+          edge => {
+            val tupleExpressionNodeId = edge.target
+            val sourcesTimeComplexes = edge.sources.map{id =>
+              val idRegex = createTimeComplexRegex(id)
+              state.graph.findRegexHyperEdges(idRegex).map(_.sources(1)).iterator
+            }
+            Programs.combineSeq(sourcesTimeComplexes.map(_.toStream)).flatMap(timeComplexSources => {
+              val (timeComplexBaseId, timeComplexEdges) = timeComplexSources.tail.foldLeft((timeComplexSources.head, Set.empty[HyperEdge[HyperTermId, HyperTermIdentifier]])){case ((baseId, edges), newId) =>
+                val tupleTimeComplexId = nodeCreator.next()
+                (tupleTimeComplexId, edges + HyperEdge[HyperTermId, HyperTermIdentifier](tupleTimeComplexId, HyperTermIdentifier(Identifier(TimeComplexRewriteRulesDB.ADD_TIME_COMPLEX)), Seq(baseId, newId), EmptyMetadata))
+              }
+              timeComplexEdges + HyperEdge(timeComplexTrueId, HyperTermIdentifier(Language.timeComplexId), Seq(tupleExpressionNodeId, timeComplexBaseId), EmptyMetadata)
+            })
+          })
+        newMatchTimeComplexEdges
     }
+  }
+
+  override def apply(state: RewriteSearchState, lastVersion: Long): (RewriteSearchState, Long) = {
+    state.graph ++= getNewEdges(state, lastVersion)
+    (state, state.graph.version)
   }
 }
