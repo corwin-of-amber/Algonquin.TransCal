a1ff6e02611111851ff3df35872322245619d614
Eytan Singher <eytan.singher@gmail.com>
2020-04-28 23:59:16 +0300

    Added more tests to coqintegration

--- src/test/scala/coqintegration/CoqAstTest.scala
+++ src/test/scala/coqintegration/CoqAstTest.scala
@@ -10,32 +10,33 @@ class CoqAstTest  extends FunSuite with Matchers{
 
   val definitionsFile = "src/main/resources/theories/Definitions.json"
   val newInductiveFile = "src/main/resources/theories/NewInductive.json"
+  val recursionFile = "src/main/resources/theories/Recursion.json"
 
   test("Definitions - find constructor names") {
     val environment = Environment.readJson(Source.fromFile(definitionsFile).mkString)
-    environment.definitions.exists(_.name.literal.endsWith("foo")) shouldEqual (true)
-    val const_number = environment.definitions.find(_.name.literal.endsWith("const_number"))
+    environment.valueDefinitions.exists(_.name.literal.endsWith("foo")) shouldEqual (true)
+    val const_number = environment.valueDefinitions.find(_.name.literal.endsWith("const_number"))
     const_number should not be empty
     const_number.get.ast.subtrees.head.root.literal should endWith ("S")
   }
 
   test("Definitions - flatten function calls") {
     val environment = Environment.readJson(Source.fromFile(definitionsFile).mkString)
-    val const_number = environment.definitions.find(_.name.literal.endsWith("const_number"))
+    val const_number = environment.valueDefinitions.find(_.name.literal.endsWith("const_number"))
     const_number should not be empty
     const_number.get.ast.root shouldNot equal (Language.applyId)
   }
 
   test("NewInductive - includes constructor definitions with name") {
     val environment = Environment.readJson(Source.fromFile(newInductiveFile).mkString)
-    val a = environment.definitions.find(_.name.literal == "A")
+    val a = environment.valueDefinitions.find(_.name.literal == "A")
     a should not be empty
     a.get.ast.expressionType should not be empty
     val typeA = a.get.ast.expressionType.get
     typeA.root.literal shouldEqual "TheSy"
     typeA.subtrees.head.root.literal shouldEqual "E"
     val thesyE = typeA
-    val b = environment.definitions.find(_.name.literal == "B")
+    val b = environment.valueDefinitions.find(_.name.literal == "B")
     b should not be empty
     b.get.ast.expressionType should not be empty
     val typeB = b.get.ast.expressionType.get
@@ -45,5 +46,19 @@ class CoqAstTest  extends FunSuite with Matchers{
     typeB.subtrees.last.subtrees.foreach({t =>
       t.shouldEqual(thesyE)
     })
+  }
+
+  test("Recursion - correctly identify function type") {
+    val environment = Environment.readJson(Source.fromFile(recursionFile).mkString)
+    val f = environment.valueDefinitions.find(_.name.literal == "f")
+    f should not be empty
+    // last checked f is length
+    f match {
+      case Some(value) =>
+        value.ast.root shouldEqual Language.mapTypeId
+        value.ast.subtrees.head shouldEqual CoqAst.TypeAst
+        value.ast.subtrees.last shouldEqual Language.applyId
+      case None => fail("Type should not be empty")
+    }
   }
 }
