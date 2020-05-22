62db325671928286f4773887d2e43dfc01995af9
Eytan Singher <eytan.singher@gmail.com>
2020-05-22 23:55:42 +0300

    Added datatype as part of transcallang for future developments

---
+++ src/main/scala/transcallang/Datatype.scala
@@ -0,0 +1,16 @@
+package transcallang
+
+/** An algebric datatype definition.
+  *
+  * @param name of datatype.
+  * @param constructors tuple of constructor name and type
+  */
+case class Datatype(name: Identifier, typeParameters: Seq[Identifier], constructors: Seq[Identifier]) {
+  // TODO: Return this once TheSy action reveives datatypes
+//  assert(constructors.forall(c => {
+//    val constructorReturnType = if (c.annotation.get.root == Language.mapTypeId) c.annotation.get.subtrees.last else c.annotation.get
+//    c.annotation.nonEmpty && constructorReturnType == asType
+//  }))
+
+  def asType: AnnotatedTree = AnnotatedTree.withoutAnnotations(name, typeParameters.map(AnnotatedTree.identifierOnly))
+}
