fc525d5517c26decb6898f75350ee6beac35d16d
Eytan Singher <eytan.singher@gmail.com>
2020-05-23 00:50:11 +0300

    Datatype type parameters should be a general expression (i.e. Tree)

--- src/main/scala/transcallang/Datatype.scala
+++ src/main/scala/transcallang/Datatype.scala
@@ -5,12 +5,12 @@ package transcallang
   * @param name of datatype.
   * @param constructors tuple of constructor name and type
   */
-case class Datatype(name: Identifier, typeParameters: Seq[Identifier], constructors: Seq[Identifier]) {
+case class Datatype(name: Identifier, typeParameters: Seq[AnnotatedTree], constructors: Seq[Identifier]) {
   // TODO: Return this once TheSy action reveives datatypes
 //  assert(constructors.forall(c => {
 //    val constructorReturnType = if (c.annotation.get.root == Language.mapTypeId) c.annotation.get.subtrees.last else c.annotation.get
 //    c.annotation.nonEmpty && constructorReturnType == asType
 //  }))
 
-  def asType: AnnotatedTree = AnnotatedTree.withoutAnnotations(name, typeParameters.map(AnnotatedTree.identifierOnly))
+  def asType: AnnotatedTree = AnnotatedTree.withoutAnnotations(name, typeParameters)
 }
