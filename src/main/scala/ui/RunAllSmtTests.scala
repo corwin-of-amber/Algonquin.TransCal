b0c297cd874c16c8cddc19e28d2aef39a3d78676
Eytan Singher <eytan.singher@gmail.com>
2020-07-14 17:38:24 +0300

    Updated distributed running to prevent double runs and deal wel with preruns.

--- src/main/scala/ui/RunAllSmtTests.scala
+++ src/main/scala/ui/RunAllSmtTests.scala
@@ -2,14 +2,16 @@ package ui
 
 import java.io.{File, FileInputStream, FileOutputStream, ObjectInputStream, ObjectOutputStream}
 
+import com.typesafe.scalalogging.LazyLogging
 import lispparser.Translator
+import synthesis.search.actions.thesy.Distributer
 import ui.Main.conf
 
 import scala.collection.mutable
 import scala.concurrent.ExecutionContext
 import scala.io.Source
 
-object RunAllSmtTests extends App {
+object RunAllSmtTests extends App with LazyLogging {
   def readRunResults(file: File): RunResults = {
     val ois = new ObjectInputStream(new FileInputStream(new File(file.getAbsolutePath + ".res")))
     val res = ois.readObject().asInstanceOf[RunResults]
@@ -19,30 +21,50 @@ object RunAllSmtTests extends App {
 
   private object Locker
 
+
   // TODO: Add timeouts by surrounding the execution with a timer inside tasks of executioncontext
   val resourcePath = "src/main/resources/ind-problems/benchmarks-dt"
-  new File(resourcePath).listFiles(_.isDirectory).par.map(d => {
-    val files = d.listFiles(f => f.isFile && f.getName.endsWith("smt2"))
-    val knowResults: mutable.Map[String, RunResults] = mutable.Map(files.collect({
-      case f if new File(f.getAbsolutePath + ".res").exists() => (f.getName, readRunResults(f))
-    }): _*)
-    // TODO: add ordering to tasks
-    val tasks = files.filterNot(f => knowResults.get(f.getName).exists(rr => rr.success)).par
-    tasks.foreach(f => {
-      val source = Source.fromFile(f)
-      val text = source.getLines().mkString("\n")
+  val allFiles = new File(resourcePath).listFiles(_.isDirectory).flatMap(d => d.listFiles(f => f.isFile && f.getName.endsWith("smt2")))
+  val knowResults: mutable.Map[String, RunResults] = mutable.Map(allFiles.collect({
+    case f if new File(f.getAbsolutePath + ".res").exists() => (f.getName, readRunResults(f))
+  }): _*)
+  val files = allFiles.filterNot(f => knowResults.get(f.getName).exists(rr => rr.successGoals == rr.goals))
+    .sortBy(_.getName.replace(".smt2", "").reverse.takeWhile(_.isDigit).reverse.toInt).par
+  val tasks = files.flatMap(f => {
+    val source = Source.fromFile(f)
+    try {
+      val res = (f, new SmtlibInterperter().toVocabAndGoals(new Translator(source.getLines().mkString("\n")).transcalScript))
       source.close()
-      val res = new Translator(text).transcalScript
-      val oosPath = f.getAbsolutePath + ".res"
-      var results = Set.empty[RunResults]
-      Locker.synchronized({
-        results = knowResults.values.toSet
-      })
-      val newRes = new ui.SmtlibInterperter().apply(res, oosPath, results)
-      Locker.synchronized({
-        knowResults(f.getName) = newRes
-      })
+      Some(res)
+    } catch {
+      case e: IllegalArgumentException =>
+        logger.warn(s"Skipping ${f.getAbsolutePath}, as it has a non equality assertion.")
+        None
+    }
+  })
+  val subTasks = tasks.flatMap(t => Distributer(t._2._1, 3).getExplorationTasks.flatten
+    .flatMap({
+      case (vocab, depth) =>
+        val newFileName = resourcePath + "/../" + "prerun_" + vocab.definitions.map(_.root.literal).sorted.mkString("_") + ".res"
+        val newFile = new File(newFileName)
+        if (newFile.exists()) {
+          knowResults(newFileName) = readRunResults(newFile)
+          None
+        }
+        else Some((newFile, (vocab, t._2._2, t._2._3)))
+    }))
+  val taskToGoal = (subTasks ++ tasks).map(t => (t._2._1, t._2._2, t._2._3, t._1)).groupBy(t => (t._1, t._2))
+  taskToGoal.keys.toArray.sortBy(_._1.definitions.size).foreach({ case k@(vocab, ruleDefs) =>
+    val f = taskToGoal(k).map(_._4).min
+    val oosPath = f.getAbsolutePath + ".res"
+    var results = Set.empty[RunResults]
+    Locker.synchronized({
+      results = knowResults.values.toSet
+    })
+    val goals = taskToGoal(k).map(_._3)
+    val newRes = new ui.SmtlibInterperter().runExploration(vocab, goals.seq.flatten.toSet, ruleDefs, oosPath, results)
+    Locker.synchronized({
+      knowResults(f.getName) = newRes
     })
   })
-
 }
--- src/main/scala/ui/SmtlibInterperter.scala
+++ src/main/scala/ui/SmtlibInterperter.scala
@@ -12,55 +12,37 @@ import synthesis.search.rewrites.Template.TemplateTerm
 import transcallang.{AnnotatedTree, Datatype, Identifier, Language}
 
 class SmtlibInterperter {
-  val state = new ActionSearchState(Programs.empty, Set.empty[RewriteRule])
-  val datatypes = collection.mutable.Set.empty[Datatype]
-  val knownFunctions = collection.mutable.Set.empty[AnnotatedTree]
-  var goal: Option[(AnnotatedTree, AnnotatedTree)] = None
-  val knownDefs = collection.mutable.Set.empty[AnnotatedTree]
+  def runExploration(vocab: SortedVocabulary, goals: Set[(AnnotatedTree, AnnotatedTree)], knownDefs: Set[AnnotatedTree], oosPath: String, previousResults: Set[RunResults]) = {
+    val relevantResults = previousResults.filter(rr => rr.knownRulesDefs.diff(knownDefs).isEmpty && rr.knownTypes.diff(vocab.datatypes.toSet).isEmpty)
 
-  //  val ois = new ObjectInputStream(new FileInputStream("temp"))
-  //  var obj: AnyRef = null
-  //  var cont = true
-  //  while (cont) {
-  //    try {
-  //      obj = ois.readObject()
-  //    } catch {
-  //      case _: Throwable => cont = false
-  //    }
-  //    assert(obj != null)
-  //    obj match {
-  //      case x: AnnotatedTree => println(s"${Programs.termToString(x)}")
-  //    }
-  //  }
-  def runExploration(t: AnnotatedTree, oosPath: String, previousResults: Set[RunResults]) = {
-    assert(t.subtrees.head.root == Language.letId)
-
-    val relevantResults = previousResults.filter(rr => rr.knownRulesDefs.diff(knownDefs).isEmpty && rr.knownTypes.diff(datatypes).isEmpty)
+    val state = new ActionSearchState(Programs.empty, Set.empty[RewriteRule])
+    knownDefs.foreach(t => new LetAction(t, allowExistential = false)(state))
     for (rr <- relevantResults) {
       state.addRules(rr.newRules.flatMap(new LetAction(_, allowExistential = false).rules))
     }
-
-    def cleanAutovar(identifier: Identifier) =
-      if (identifier.literal.startsWith("?autovar")) identifier.copy(literal = identifier.literal.drop(1))
-      else identifier
-
-    goal = Some((t.subtrees.head.subtrees(0).map(cleanAutovar), t.subtrees.head.subtrees(1).map(cleanAutovar)))
-    val vocab = SortedVocabulary(datatypes.toSeq, knownFunctions.toSeq)
     val exampleDepth = 3
     val distributer = Distributer(vocab, exampleDepth)
     distributer.runTasks(state)
     val thesy = new TheoryExplorationAction(vocab, exampleDepth, None, None, None, None, None, true)
-    thesy.addGoal(goal.get)
+    goals.foreach(g => thesy.addGoal(g))
     thesy(state)
-    val res = RunResults(datatypes.toSet, knownFunctions.toSet, knownDefs.toSet, distributer.foundRules ++ thesy.getFoundRules, goal, thesy.goals.isEmpty)
+    val res = RunResults(vocab.datatypes.toSet, vocab.definitions.toSet, knownDefs.toSet, distributer.foundRules ++ thesy.getFoundRules, goals, goals.diff(thesy.goals))
     val oos = new ObjectOutputStream(new FileOutputStream(oosPath))
     oos.writeObject(res)
     oos.close()
     res
   }
 
-  def apply(terms: List[AnnotatedTree], oos: String, previousResults: Set[RunResults] = Set.empty): RunResults = {
-    var res: Option[RunResults] = None
+  def toVocabAndGoals(terms: List[AnnotatedTree]): (SortedVocabulary, Set[AnnotatedTree], Set[(AnnotatedTree, AnnotatedTree)]) = {
+    val datatypes = collection.mutable.Set.empty[Datatype]
+    val knownFunctions = collection.mutable.Set.empty[AnnotatedTree]
+    var goal: Option[(AnnotatedTree, AnnotatedTree)] = None
+    val knownDefs = collection.mutable.Set.empty[AnnotatedTree]
+
+    def cleanAutovar(identifier: Identifier) =
+      if (identifier.literal.startsWith("?autovar")) identifier.copy(literal = identifier.literal.drop(1))
+      else identifier
+
     for (t <- terms) {
       t.root match {
         case Language.functionDeclId =>
@@ -71,15 +53,24 @@ class SmtlibInterperter {
           datatypes += Datatype(t.subtrees.head.root, Seq.empty, t.subtrees.tail.map(_.root))
         case Language.letId =>
           knownDefs += t
-          val letAction: LetAction = new LetAction(t, allowExistential = false)
-          letAction(state)
         case Language.assertId =>
-          res = Some(runExploration(t, oos, previousResults))
+          assert(t.subtrees.head.root == Language.letId)
+          goal = Some((t.subtrees.head.subtrees(0).map(cleanAutovar), t.subtrees.head.subtrees(1).map(cleanAutovar)))
         case Identifier("not", annotation, namespace) if t.subtrees.head.root == Language.letId =>
-          res = Some(runExploration(t, oos, previousResults))
-        case x => throw new NotImplementedError("Check this")
+          assert(t.subtrees.head.root == Language.letId)
+          goal = Some((t.subtrees.head.subtrees(0).map(cleanAutovar), t.subtrees.head.subtrees(1).map(cleanAutovar)))
+        case Identifier("not", annotation, namespace) if t.subtrees.head.root != Language.letId =>
+          throw new IllegalArgumentException("Don't support stuff that isn't equality")
+        case x =>
+          throw new NotImplementedError("Check this")
       }
     }
-    res.get
+    val vocab = SortedVocabulary(datatypes.toSeq, knownFunctions.toSeq)
+    (vocab, knownDefs.toSet, Set(goal.get))
+  }
+
+  def apply(terms: List[AnnotatedTree], oos: String, previousResults: Set[RunResults] = Set.empty): RunResults = {
+    val (vocab, funcDefs, goals) = toVocabAndGoals(terms)
+    runExploration(vocab, goals, funcDefs, oos, previousResults)
   }
 }
