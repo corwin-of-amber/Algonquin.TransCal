138db2f5fd934b6123b88119dc8ad6db71f26984
Eytan Singher <eytan.singher@gmail.com>
2020-07-19 12:25:46 +0300

    ConjectureChecker now caches previous graphs and results to save time in future comparisons.

--- src/main/scala/synthesis/search/actions/thesy/ConjectureChecker.scala
+++ src/main/scala/synthesis/search/actions/thesy/ConjectureChecker.scala
@@ -1,11 +1,16 @@
 package synthesis.search.actions.thesy
 
+import java.util.UUID
+
 import com.typesafe.scalalogging.LazyLogging
 import report.{LazyTiming, StopWatch}
-import synthesis.Programs
+import structures.HyperEdge
+import structures.mutable.CompactHyperGraph
+import synthesis.{HyperTermId, HyperTermIdentifier, Programs, search}
 import synthesis.search.actions.{Action, ObservationalEquivalence, SearchAction}
 import synthesis.search.rewrites.RewriteRule
-import synthesis.search.{ActionSearchState, Operator}
+import synthesis.search.rewrites.RewriteRule.HyperGraph
+import synthesis.search.{ActionSearchState, Operator, rewrites}
 import transcallang.{AnnotatedTree, Identifier, Language}
 
 import scala.collection.mutable
@@ -29,6 +34,7 @@ class ConjectureChecker(prover: Prover, searcher: SearchAction, maxDepth: Double
   private val foundRules = mutable.Set.empty[RewriteRule]
   private val failedAttempts = mutable.Buffer.empty[(AnnotatedTree, AnnotatedTree)]
   private var retriedProofs = 0
+  private val oe = new ObservationalEquivalence(searcher)
 
   protected def retryFailed: Set[(AnnotatedTree, AnnotatedTree)] = {
     var i = 0
@@ -37,8 +43,8 @@ class ConjectureChecker(prover: Prover, searcher: SearchAction, maxDepth: Double
     while (i < failedAttempts.length) {
       val (term1, term2) = failedAttempts.head
 
-      val newState = searcher(new ActionSearchState(Programs(term1).addTerm(term2), prover.knownRules), Some(maxDepth))
-      if (newState.programs.findTree(term1).intersect(newState.programs.findTree(term2)).nonEmpty) {
+      val eqs = oe.fromTerms(Seq(term1, term2), prover.knownRules, maxDepth)
+      if (eqs.size == 1) {
         failedAttempts.remove(i)
       } else {
         val res = prover.inductionProof(term1, term2) //.collect({ case rr => rr })
@@ -56,6 +62,9 @@ class ConjectureChecker(prover: Prover, searcher: SearchAction, maxDepth: Double
     found.toSet
   }
 
+  val checkCache: mutable.Map[AnnotatedTree, (HyperTermIdentifier, rewrites.RewriteRule.HyperGraph)] =
+    mutable.Map.empty[AnnotatedTree, (HyperTermIdentifier, rewrites.RewriteRule.HyperGraph)]
+
   /** This includes both the conjecture screening and the proving as they are highly coupled.
     *
     * @param conjectures equality classes from soe
@@ -67,7 +76,30 @@ class ConjectureChecker(prover: Prover, searcher: SearchAction, maxDepth: Double
       case terms if terms.size > 1 =>
         // refine equivalence relation using searcher
         logger.info("Filtering terms that don't need induction using observational equivalence")
-        val finerEquives = new ObservationalEquivalence(searchAction = searcher).fromTerms(terms.toSeq, prover.knownRules, maxDepth)
+        val graphs = terms.map(t => {
+          if (!checkCache.contains(t)) {
+            checkCache(t) = {
+              val anchor = Identifier(UUID.randomUUID().toString)
+              (HyperTermIdentifier(anchor), Programs.destruct(AnnotatedTree.withoutAnnotations(Language.andCondBuilderId,
+                List(t, AnnotatedTree.identifierOnly(anchor)))))
+            }
+          }
+          checkCache(t)._2
+        }).toSet
+        // TODO: remove once I see it holds
+        assert(graphs.toSeq.combinations(2).forall({ x: Seq[HyperGraph] => x.head.edges != x.last.edges }))
+
+        def moveEdge(n: Int, hyperEdge: HyperEdge[HyperTermId, HyperTermIdentifier]) =
+          hyperEdge.copy(target = hyperEdge.target.copy(hyperEdge.target.id + n), sources = hyperEdge.sources.map(s => s.copy(s.id + n)))
+
+        val finerEquives = {
+          val updatedGraph = graphs.reduce[HyperGraph]({ case (g1, g2) => g1 ++= g2.map(moveEdge(g1.nodes.map(_.id).max, _)) })
+          terms.foreach(t => checkCache(t) = (checkCache(t)._1, updatedGraph))
+          val state = oe(new search.ActionSearchState(updatedGraph, prover.knownRules), Some(maxDepth))
+          val mapping = terms.groupBy(t => state.programs.queryGraph.findByEdgeType(checkCache(t)._1).head.target)
+          mapping.values.toSet
+        }
+
         // Take smallest of each set as heuristic to maximize knowledge gained from each theorem
         val representatives = finerEquives.map(_.minBy(_.size)).toSeq
         representatives.combinations(2).toSeq.flatMap(it => {
@@ -90,13 +122,19 @@ class ConjectureChecker(prover: Prover, searcher: SearchAction, maxDepth: Double
             Set.empty[(AnnotatedTree, AnnotatedTree)]
           }
         })
-    }).toSet flatMap ((trees: Seq[(AnnotatedTree, AnnotatedTree)]) => trees.map({case (t1,t2) => AnnotatedTree.withoutAnnotations(Language.letId, Seq(t1, t2))}))
+    }
+
+    ).toSet flatMap ((trees: Seq[(AnnotatedTree, AnnotatedTree)]) => trees.map({
+      case (t1, t2) => AnnotatedTree.withoutAnnotations(Language.letId, Seq(t1, t2))
+    }))
   }
 
   def prettify(term: AnnotatedTree) = {
     val phs = term.nodes.map(_.root).filter(_.literal.startsWith("?"))
     term.map((v: Identifier) => phs.indexOf(v) match {
-      case i if i >= 0 => new Identifier(s"?${i}")
+      case i if i >= 0 => new Identifier(s"?${
+        i
+      }")
       case _ => v
     })
   }
